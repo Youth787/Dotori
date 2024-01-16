bcef85eb8bf007e09b95bd06075f75bc134416bf
SeokHyun Cho <doongdang@naver.com>
2024-01-17 00:07:09 +0900

    feat : mdi, fontawesome, ionicons

--- Frontend/quasar.config.js
+++ Frontend/quasar.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:445f6592c34e0a99921ce66a615e21ad28132c44bec635bcee6311a0879599d1
-size 6118
+oid sha256:0cffa0af06b643d616d7c2b33f90c44447217c27d253f6a5692b968f28623b9d
+size 6087
