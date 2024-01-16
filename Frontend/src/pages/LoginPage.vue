cddc20b12bae19cfa7e2405aa281882277c4cd74
문준형 <moonabcd@naver.com>
2024-01-16 17:45:33 +0900

    design : 로그인창 요소배치완성
    Former-commit-id: 261d32ae6c01449a030633f26da4f99626889e2a

--- Frontend/src/pages/LoginPage.vue
+++ Frontend/src/pages/LoginPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1dc5142a0757ae758ead23877f70ed3d546ff6ad57bf329568d36cab13d7890
-size 831
+oid sha256:9dd6457a2f8ba307dc21f80fda8cc5d90fe9ce3c0e57c10b689977f2c4dcbed9
+size 2496
