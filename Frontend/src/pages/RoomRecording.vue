89f95cd5769afc51f902a24817a35951988d5ddb
SeokHyun Cho <doongdang@naver.com>
2024-01-17 01:31:43 +0900

    feat : Scene 스크롤바
    Former-commit-id: 35a8d0bdc115a0bb516143ede1583016bb67907f

--- Frontend/src/pages/RoomRecording.vue
+++ Frontend/src/pages/RoomRecording.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc32fcc82b4bd4be3f7ca6ff56f8ce7ca204e8c9e46a712d4cf7c99b5300f1ca
-size 1935
+oid sha256:9199e4467917adcfe8c6d1fe3ccf592f1f0909cb1cace766866e10188346399f
+size 3034
