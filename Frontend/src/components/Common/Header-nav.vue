f766b25222dd767bf48d69fc88186f3a88300901
yeonmi <wkdus885@naver.com>
2024-01-16 15:31:23 +0900

    style : add vue

---
+++ Frontend/src/components/Common/Header-nav.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7be22bbddb5ee8de5fc05cc9a29a6960768e23a7df1a29cfa8c5dcf1e221f189
+size 249
--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f61f6448395c24a728d92e54bad469414767cc248565d77283b8151b29cd28a
-size 962
+oid sha256:6d9d374f0fd2034ebd295b36b4b3620f6804ca56936fe3773ab82bfe4568c2c4
+size 300
