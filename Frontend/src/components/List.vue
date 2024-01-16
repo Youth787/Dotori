2c921a744ce3944f318eebc7cd337067c0cfa29f
yeonmi <wkdus885@naver.com>
2024-01-16 15:50:43 +0900

    style : add vue components

--- Frontend/src/components/List.vue
+++ Frontend/src/components/List.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0239a396b2cf2d0d842ffbc111c7265d7e97c2bfa7f9b7301a398e21ee016a4b
-size 642
+oid sha256:ca217080354a004ae634e5c1e39552df288e55d9548fcc47f95542434e8ab1b3
+size 778
