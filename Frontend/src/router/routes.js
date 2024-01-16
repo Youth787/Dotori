a8ab670f72f9d6b485920a6981d7ef925aa53a9c
SeokHyun Cho <doongdang@naver.com>
2024-01-16 17:23:20 +0900

    feat : 방 녹화 페이지 router

--- Frontend/src/router/routes.js
+++ Frontend/src/router/routes.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3972d3286709e9a2a6076e6b07833ca849fd0f6224cde7198c6e0fc17b900dc
-size 120
+oid sha256:b33e8b1f54d064eac1016f35a484149ad8831b2e499c082c93748823e810147d
+size 214
