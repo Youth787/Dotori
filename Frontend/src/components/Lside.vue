7cb231b27e7f81668fe6648c3d9d00575eb63a92
yeonmi <ar77gt@daum.net>
2024-01-16 15:50:43 +0900

    style : add vue components

--- Frontend/src/components/Lside.vue
+++ Frontend/src/components/Lside.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f95c7f184a4c0e44454b8fb8190ff0cf46d7c9dac5de2aada011daada095173f
-size 293
+oid sha256:dc66209f979194335c82a14326ba7fa848d7788979710c7fe98c4069aa048956
+size 183
