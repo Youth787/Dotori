724e16309cba772ecec4edcc12e18a5987c9e509
이하은 <hahi1108@gmail.com>
2024-01-17 21:12:06 +0900

    design : 방 생성 페이지 폰트 변경 및 배경 색상 추가
    Former-commit-id: ea1552c717bd50590e4ca5243044be5cc86af369

--- Frontend/src/pages/RoomMakingModal.vue
+++ Frontend/src/pages/RoomMakingModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:084967dab504fde8b4cc38d0eb80f6bcdfd15551ae5be67495d16284db78f329
-size 4028
+oid sha256:b7769ab51146fee0f595487490d5dee953f3d452d5ed834b4c69454c49483fd5
+size 4669
