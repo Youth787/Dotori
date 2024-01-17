08f71824129047104e1094392157ff28e3ae6403
이하은 <hahi1108@gmail.com>
2024-01-17 22:13:29 +0900

    feat : 반응형 변수로 변환
    Former-commit-id: 42223e076d8da61b683a79cd05e96d5e89d9409d

--- Frontend/src/components/Character.vue
+++ Frontend/src/components/Character.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dccd04ac6185c046bddc448ead4f1bfc36198faf82bb3adcb5916c1e9559f68d
-size 358
+oid sha256:2cce8a9664196c7a5508954a0e9655985ae05749d630dadffa4f7f38c04b2a8e
+size 420
