145ddea212da78b51a3f0666b5e52f00a5737c90
이하은 <hahi1108@gmail.com>
2024-01-17 20:16:57 +0900

    feat : 방 생성 페이지 디자인 통일
    Former-commit-id: 63114b4062ec522fa5db412c458fcc72facc7bed

--- Frontend/src/pages/RoomMakingModal.vue
+++ Frontend/src/pages/RoomMakingModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2ef66fe8709cd3cb431fe7ab1b323b4e55bc266b67b89cad2a116dd4fd3b340
-size 2109
+oid sha256:960647908b460fa594175cebb84c96fd50521ccc3661fd3aff18acc4b69b9b98
+size 3558
