48b1ad23ef3dd99def232e99d8d743558c7acad1
yeonmi <ar77gt@daum.net>
2024-01-17 16:13:47 +0900

    feat : q-list 추가
    Former-commit-id: 3de27b9e01914cda4aa58715074dd563da48bfd5

--- Frontend/src/layouts/List/ListDrawer.vue
+++ Frontend/src/layouts/List/ListDrawer.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e20a4f86f359b119ed1acdce0c1b9d58065cecd8ee3f91e6d166c03a1c200148
-size 120
+oid sha256:e436d998904a37ad43ed3a3d132121c9bb35a0bf6622fea13dd14bdc77f7c6ab
+size 1346
