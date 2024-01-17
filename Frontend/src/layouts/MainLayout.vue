4741d8e59bbca25f1c5b0bc78eeb7e299478bb9f
yeonmi <ar77gt@daum.net>
2024-01-17 13:34:22 +0900

    add : files
    Former-commit-id: 8ba0a2293429a204aa8eac7efb1e405b214078e7

--- Frontend/src/layouts/MainLayout.vue
+++ Frontend/src/layouts/MainLayout.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c43704d131eb989e827e9c75efb499ecbfbc29574242cbbb7687f1043274c8cb
-size 1424
+oid sha256:66078accfce232f90cd49cb7a418e27e991c402df9df8bb6dad6981ff44b1233
+size 1453
