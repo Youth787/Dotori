41c8907e522fe4cf9309e5003af9514545e75060
yeonmi <wkdus885@naver.com>
2024-01-17 17:33:50 +0900

    add : list indexpage 추가
    Former-commit-id: acd42e8df9a0f166688fff96b503fe647d53754b

--- Frontend/src/pages/List/IndexPage.vue
+++ Frontend/src/pages/List/IndexPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a01abd4596fe515b048fd75807c814e44f487c26a19883fc2f31efd476ccd2b
-size 565
+oid sha256:2a0e6c62dede33e7829f9afbd6486871bd9e963ba1f957041a45ec001bd7bd1a
+size 875
