ab69dd7d0796a6258645edda1b2fad7aa41b67b8
yeonmi <wkdus885@naver.com>
2024-01-17 11:31:37 +0900

    add : breakpoints

--- Frontend/src/components/EssentialLink.vue
+++ Frontend/src/components/EssentialLink.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47e35546715a4b65fa0ce55cec9e5d637151c7ea389685aa54f6808db238c675
-size 714
+oid sha256:15a58c502318f80ca6806563034c5afe7d67084c0ef0f0a37e60417de337f696
+size 875
--- Frontend/src/layouts/MainLayout.vue
+++ Frontend/src/layouts/MainLayout.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1f1e110a5a173b4167a144238ac75fc21dae5f71e17f1ef813a39dc3db14276
-size 2227
+oid sha256:c43704d131eb989e827e9c75efb499ecbfbc29574242cbbb7687f1043274c8cb
+size 1424
