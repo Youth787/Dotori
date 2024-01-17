e3be494f2d09e1ed2a180c93ab418c35980c9484
yeonmi <wkdus885@naver.com>
2024-01-17 16:21:11 +0900

    feat : left sidebar 추가
    Former-commit-id: 82dbbb554d114f3f5310d47e36528dfcc0b449bb

--- Frontend/src/layouts/List/ListDrawer.vue
+++ Frontend/src/layouts/List/ListDrawer.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e436d998904a37ad43ed3a3d132121c9bb35a0bf6622fea13dd14bdc77f7c6ab
-size 1346
+oid sha256:07c7e984691e5911e8da6ab3d459be668d2acb7e3a9370d709f8c2eb5a4a0bcc
+size 2003
