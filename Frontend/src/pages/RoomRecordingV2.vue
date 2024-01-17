96fd48c0c1a3095543f4c3c4ef4bce95d68ac9a3
SeokHyun Cho <doongdang@naver.com>
2024-01-17 14:13:35 +0900

    feat : 스크립트와 버튼 디자인 변경 버전
    Former-commit-id: 9278e059cd7e6eb110c637517ffc283b13a91dcc

---
+++ Frontend/src/pages/RoomRecordingV2.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:63635df0be4f6b6a1da4d7b26d8507dbd1f8980335786db5944e240b2b7a584f
+size 6086
--- Frontend/src/router/routes.js
+++ Frontend/src/router/routes.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b9f0131d682fd794ce60bd6f61ae34f9d21cbfb7df8d7d41ce240a9f2c75f3e
-size 798
+oid sha256:fdc9491cd0298db7308e621352546fe80a15928f03024e106ecb14f5b2aca535
+size 897
