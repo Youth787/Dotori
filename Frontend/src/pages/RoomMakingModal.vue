fe947208918df37b4a5fe4ef344c49d13befc993
이하은 <hahi1108@gmail.com>
2024-01-17 22:13:29 +0900

    feat : 반응형 변수로 변환
    Former-commit-id: 42223e076d8da61b683a79cd05e96d5e89d9409d

--- Frontend/src/pages/RoomMakingModal.vue
+++ Frontend/src/pages/RoomMakingModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7769ab51146fee0f595487490d5dee953f3d452d5ed834b4c69454c49483fd5
-size 4669
+oid sha256:1cc86623b29c229d3ac98f6bdf0129493334235e6e17b7f12c4b829d1a185b87
+size 5064
