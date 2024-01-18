300a453b74539da19cbb06b5897c3dab3f486d12
doongdang <whtjrgus0428@gmail.com>
2024-01-18 20:11:58 +0900

    remove : 미사용 녹화 페이지
    Former-commit-id: 8bb111108e716be0d9690d91b53f12a226d5cb20

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0205e16707008a3abe88b460ec6c3f45ce52241b550c52ceb2256a6909be2322
-size 154
+<template>
+  <router-view />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+
+});
+</script>
--- Frontend/src/pages/RoomRecordingV3.vue
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1e1bd67d749cc1b66849da5fedb61eaa14162433207fd9e8fb0519ca255a8686
-size 7005
