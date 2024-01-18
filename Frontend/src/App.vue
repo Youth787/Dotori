52fb3f31dd55fb16ac0700266116fde2f844b685
yeonmi <wkdus885@naver.com>
2024-01-18 11:38:25 +0900

    feat : add booklist
    Former-commit-id: 15018f66668fd10eaee7ed7654b32920cc04b181

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,11 +1,3 @@
-<template>
-  <router-view />
-</template>
-
-<script>
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'App',
-});
-</script>
+version https://git-lfs.github.com/spec/v1
+oid sha256:0205e16707008a3abe88b460ec6c3f45ce52241b550c52ceb2256a6909be2322
+size 154
