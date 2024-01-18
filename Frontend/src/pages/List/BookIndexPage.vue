0a9a6582ee975ad948bb0100fa1ac84cb03c2d50
yeonmi <wkdus885@naver.com>
2024-01-18 11:38:25 +0900

    feat : add booklist
    Former-commit-id: 43440d70aaaab8e1e0cb65539a9bec7cb58570a4

--- Frontend/src/pages/List/BookIndexPage.vue
+++ Frontend/src/pages/List/BookIndexPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97bcba8090f862fa8dfe9800d4358552046a822d27763d1e71ee243c28e6211c
-size 2226
+oid sha256:8a043ef55b697bc92ad6fbd3552db2fad6115025c1477b0c7e263a43d6575b64
+size 1723
