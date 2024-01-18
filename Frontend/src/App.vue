1ea53c9603a05c0389bfeeca19ef672825660b8b
jh <bndgc@naver.com>
2024-01-18 17:23:21 +0900

    Feat: MyPageLayout
    Former-commit-id: e47fa9bf0e7717e28e17477d546839004876dcb2

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:015d10119ead1844c9b09565fb6dd9e69a5f0425c549443c69d4114b2b683091
-size 62
+<template>
+  <router-view />
+</template>
+
+<script>
+
+</script>
