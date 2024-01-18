c363d4bdd099b36aae39ab389de8d2529150f632
yeonmi <wkdus885@naver.com>
2024-01-18 10:09:36 +0900

    feat : add buttons
    Former-commit-id: 4d7498b641f1003fdb16984eebbf2a9f6a536ca5

--- Frontend/src/router/routes.js
+++ Frontend/src/router/routes.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5d1e2b289a30d4692fd2af9c4c7024ac94ace9624357e435ae500b45c087a06
-size 622
+oid sha256:601523e402b1d51a774cc4541e1759056399e9fe6ad0ef1b671ef1098a62f01a
+size 804
