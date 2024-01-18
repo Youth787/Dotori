c734879850417be8096e5708bf1346809ac255d5
문준형 <moonabcd@naver.com>
2024-01-18 17:30:35 +0900

    design : 메인페이지 구조추가(기능,사진 미완)

--- Frontend/src/layouts/MainLayout.vue
+++ Frontend/src/layouts/MainLayout.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6312cdd7e39ef270768d6effe050212276a34dc2276bed2132841aa901675a9d
-size 172
+oid sha256:c650c698d78830b2d49b9cb63e1a70d157309572ab0db02ea7270312c32d5f3b
+size 4156
