95710358a391852341748859f1292365d8815b5f
jinooyang <jinooyang@gmail.com>
2024-01-19 11:19:45 +0900

    design : mypage
    Former-commit-id: 8674924b82e774425513035b6e6b95a69faae216

--- Frontend/src/layouts/MyPageLayout.vue
+++ Frontend/src/layouts/MyPageLayout.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:caa40078a565c8c7e2f41d76271295120f766d21d6498ccbc4e96a0758e76f98
-size 2939
+oid sha256:6a571f6298aabc71628928bbf0979d72b0acbd496024b4f3ebb5a02a56d6ca21
+size 3001
