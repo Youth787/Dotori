c470089b8e03d7334922d7b67bc148a1fc0164c7
jinooyang <jinooyang@gmail.com>
2024-01-19 11:19:45 +0900

    design : mypage
    Former-commit-id: bb4985f4f0ccfc97306d199af2f250cf91d23e77

--- Frontend/src/pages/MyInfoPage.vue
+++ Frontend/src/pages/MyInfoPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a9c25588338d070b1f46e1c03cdd46bd118c3e2a45c536c10462829996b76e0
-size 2155
+oid sha256:c1398e06ca446a5e8394799dd1d6a1bb86fa3ffebe79b7789dbe9dfbadaf5117
+size 2321
