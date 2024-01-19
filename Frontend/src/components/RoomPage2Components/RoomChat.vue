2747a0aabb82c1b2b34d38a6c966b7d10264167b
jinooyang <jinooyang@gmail.com>
2024-01-19 17:31:15 +0900

    design refactor all pages
    Former-commit-id: 7444125a80a611fd548477871acf44514f1a40e8

---
+++ Frontend/src/components/RoomPage2Components/RoomChat.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cdbb66897f21b930644fee23659a6a8f48ba0fb2ee2f48fcb993aac8e80829e7
+size 1542
--- Frontend/src/pages/MyAvatarPage.vue
+++ Frontend/src/pages/MyAvatarPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69edac4d469d08d399cdf8066880ea9c406d169b90f116f2f3e78bf94114d51c
-size 1017
+oid sha256:bb8f180407ccc09be4eb7abe57e2e169e6c4a976fab2643eb4df304295ce926a
+size 1755
