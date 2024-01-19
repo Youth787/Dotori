7477aa568a304d23cbbe2fda2a58d55f3c8e0ff1
SeokHyun Cho <doongdang@naver.com>
2024-01-19 11:19:54 +0900

    feat : Loading Plugin
    Former-commit-id: e53c1a1f668effb2821daa807b50f673fa9b2add

--- Frontend/quasar.config.js
+++ Frontend/quasar.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84f39df76dedbdc11d7d80341fde2d7a911a93897c43bcace718a13a26488f41
-size 6080
+oid sha256:10c22ee58421ef27d8860b2a23ec2090bdbcc0bd466cf2748117b0211e07b175
+size 6090
