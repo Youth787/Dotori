5b13a144a23bbac607646f123e601e91864156f3
SeokHyun Cho <doongdang@naver.com>
2024-01-22 15:05:44 +0900

    # <타입> : <제목> 형식으로 작성하며 제목은 최대 50글자 정도로만 입력 ################ # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성
    ################
    # 본문(추가 설명)을 아랫줄에 작성
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (관련된 이슈 번호 등 추가)
    
    ################
    #   [ 커밋 타입 목록 ]
    #   feat : 기능 (새로운 기능)
    #   fix : 버그 (버그 수정)
    #   design : CSS 등 사용자 UI 디자인 변경
    #   refactor : 리팩토링
    #   style : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음)
    #   docs : 문서 (문서 추가, 수정, 삭제)
    #   add : 코드나 테스트, 예제, 문서등의 추가 생성이 있는 경우
    #   test : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음)
    #   chore : 기타 변경사항 (빌드 스크립트 수정ea 등)
    #   post : 블로그 포스트 추가 (신규 포스트 작성 및 수정)
    #   rename : 파일 혹은 폴더명을 수정하거나 옮기는 작업만 하는 경우
    #   remove : 파일을 삭제하는 작업만 수행한 경우
    #   move : 코드의 이동이 있는 경우
    #   comment : 필요한 주석 추가 및 변경
    #   update : 계정이나 버전 업데이트가 있는 경우
    ################
    
    
    Former-commit-id: 80cc5cf852cf651e61509e81df3a3f77da584e38

--- Frontend/src/router/routes.js
+++ Frontend/src/router/routes.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bc4a437e12b4422d1a0a7431b36b0f8e06ee2f4722aa6971b07d65e60200127
-size 323
+oid sha256:639737e872f31c23f64ebcb87a7d43b5fa50c7609c40bfc1437a5cf8c62f0ed3
+size 705
