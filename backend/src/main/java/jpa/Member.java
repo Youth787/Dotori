2301c098bd00b3ddb4953f2856df04a14110239c
yeonmi <ar77gt@daum.net>
2024-01-22 13:29:35 +0900

    feat : add db
    Former-commit-id: a380e1bb519f6babf52f55480d510395ebcefaed

---
+++ backend/src/main/java/jpa/Member.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c001d6a669708b0fea5a8a0b73bf836e8c40ada51516b75986612091bd6db032
+size 771
---
+++ backend/src/main/java/jpa/jpamain.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:346e6ad222beb4193672040f43f301c137e4e1daec6a71f4e8548526a8405035
+size 1306
