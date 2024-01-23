34cdde4827ce35bf5d5704021ec74240a33076a1
yeonmi <wkdus885@naver.com>
2024-01-23 15:41:58 +0900

    feat : jpa complete
    Former-commit-id: c40b8f018d362d9d083157ee92ba8957936cedc8

--- backend/src/main/java/com/dotori/backend/entity/RoomEntity.java
+++ backend/src/main/java/com/dotori/backend/entity/RoomEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:956a49a2f8099618eb4c0f94fef81b79047fa674ba647312fe6be57311b243e9
-size 1116
+oid sha256:5eb2af7e47167315b04a0cb82f9ea1f648b5f73eba48bf577d0ab13c4715ed2e
+size 1316
---
+++ backend/src/main/java/com/dotori/backend/service/BookService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d99caa2b9d989a3d6c746835d49bf66c590629a38e76a3637a37014c38f4c005
+size 691
