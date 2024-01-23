e7ce3f136d5d5a9843da29b960482db11b0330f3
yeonmi <ar77gt@daum.net>
2024-01-23 15:43:01 +0900

    feat: jpa complete
    Former-commit-id: d36b7f3c27ee804834a7624d94086676243e15d7

--- backend/src/main/java/com/dotori/backend/entity/RoleEntity.java
+++ backend/src/main/java/com/dotori/backend/entity/RoleEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4dbee5e7e9dd14c0e403dd149c3c437d18884bd27b4617ef2ecc3503339dd248
-size 457
+oid sha256:a08d9eed34019e9583b54c0d7daec995d3ea569a3b42ae94e0e9aec31bc89379
+size 488
--- backend/src/main/java/com/dotori/backend/entity/ScriptEntity.java
+++ backend/src/main/java/com/dotori/backend/entity/ScriptEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aeb37f7af87535f0c22cf000feffd9ce7d27d2489328d795b05349554ec93f63
-size 672
+oid sha256:645332058202b9c2aa1892d06a8328579f1f4409f6f54883f3e5ffb36ce72b9f
+size 703
