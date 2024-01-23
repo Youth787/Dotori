ea4291d6c143829aaebd27dcc13b708985b2bb15
문준형 <moonabcd@naver.com>
2024-01-23 17:43:17 +0900

    add : !잘못된! 프론트 코드 수정
    Former-commit-id: a68cfe1757b2348500c4f10253fdbdc3304df558

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93216c123c763aa3922b6f49169e225cffcc8fd51ec71a5854909449b7741cdb
-size 985
+oid sha256:53a0fd6b67264e09581914264b9d9745fe3dff8effd77147697455d910c69ea7
+size 1033
