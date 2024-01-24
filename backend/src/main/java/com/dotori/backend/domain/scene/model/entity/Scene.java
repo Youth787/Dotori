659d1522ad6780a2884744b281e7622ff444ffbd
jh <bndgc@naver.com>
2024-01-24 16:32:05 +0900

    mod: String length 제한 추가
    Former-commit-id: 002e187a0f873bd70292de09fbc3060a2334e0a7

--- backend/src/main/java/com/dotori/backend/domain/scene/model/entity/Scene.java
+++ backend/src/main/java/com/dotori/backend/domain/scene/model/entity/Scene.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:732f25e2218376beda3112f1dd906a3eb9ceb5abe61995809f017617ac9d3e76
-size 1044
+oid sha256:3a23fa49e8427164392281e62c6f6f652d8eef372f29f27d69fca71f585e5133
+size 1058
