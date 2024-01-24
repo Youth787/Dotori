91697584510c59154bcac1f5b89d1f26a0e5deb6
SeokHyun Cho <doongdang@naver.com>
2024-01-24 12:04:02 +0900

    style : Camel, Snake 통일
    Former-commit-id: a8358936e98688b6cd652538955b9a45c0d81a8d

--- backend/src/main/java/com/dotori/backend/entity/Member.java
+++ backend/src/main/java/com/dotori/backend/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0416e8a35828c6d11c9900a0836136c7fdfff9d203635507d2618fccdca67fe7
+oid sha256:c3acd44c819697771a681cc2d170452bf7d67638a44da0788b0005c4f26dc3d5
 size 570
--- backend/src/main/java/com/dotori/backend/entity/RoomMember.java
+++ backend/src/main/java/com/dotori/backend/entity/RoomMember.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8144377770c4df05dea8077eeab4ff44987195aa34eb29033307c6f713d39bec
+oid sha256:78a30c653663d5697b694aad328d2f670bd610510db974f85d11d52a4d13ceec
 size 838
