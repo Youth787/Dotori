8cee8dd69e55390033a261fa588509cd88e29fe2
SeokHyun Cho <doongdang@naver.com>
2024-01-24 11:08:39 +0900

    style : 생성, 수정일시 명칭 변경
    Former-commit-id: 108ce5a56c85a360a3aaad83987c7dfc2a996753

--- backend/src/main/java/com/dotori/backend/entity/BaseTimeEntity.java
+++ backend/src/main/java/com/dotori/backend/entity/BaseTimeEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:666eaffb7b33d00b32488abb530b959390aa8e7ab1930f3eafb5148a5416d55a
-size 728
+oid sha256:25ac6f393b69248e338b3d825c7b5ac3e9458ae2cc46c08bb2963aa59c9fbf05
+size 719
