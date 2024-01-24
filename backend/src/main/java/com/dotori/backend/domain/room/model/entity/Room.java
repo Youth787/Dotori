3eeb63f9e955a22a0dfab4705a89d45b86ef961a
SeokHyun Cho <doongdang@naver.com>
2024-01-24 15:22:51 +0900

    remove : 방 상태
    Former-commit-id: b14904ba1e2c50118646b09f941984c79ce78c64

--- backend/src/main/java/com/dotori/backend/domain/room/model/entity/Room.java
+++ backend/src/main/java/com/dotori/backend/domain/room/model/entity/Room.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aab09473a089e0ff2bb64462e064b82a98b227abec785c9b1cf6f47248f6b036
-size 1909
+oid sha256:915b8f14729dcbe057a77380c2c8acd0b5e93c3f0a952c21cd4de84974efaf78
+size 1852
