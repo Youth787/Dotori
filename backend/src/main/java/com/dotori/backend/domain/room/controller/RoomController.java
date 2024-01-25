875944545ed728dbc60251970d91cd14b4411769
이하은 <hahi1108@gmail.com>
2024-01-25 09:39:49 +0900

    test : openvidu connection ID 콘솔 출력 코드 주석처리
    Former-commit-id: f42249f19c1c0e9d77cf27065b758d65078b08bb

--- backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8885a3028064425b088ab1d8c583c1d92b67f856af27c2541f4414c1db2fd572
-size 2861
+oid sha256:1f56dc3d1958c3998f7c39f6dbfe0b6a888d6ca99b44c88f5c8b483a8428153e
+size 2864
