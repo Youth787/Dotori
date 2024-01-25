6ee7e88fcf1d001ff24dc9e3a3f95e4230d48592
이하은 <hahi1108@gmail.com>
2024-01-25 16:23:28 +0900

    cofix : 방 ì 엔티티 매니저 사용한 방 정보 저장 코드 주석 해제
    Former-commit-id: cb136a1667871ac1bca62b6eca382076c5377647

--- backend/src/main/java/com/dotori/backend/domain/room/repository/RoomRepository.java
+++ backend/src/main/java/com/dotori/backend/domain/room/repository/RoomRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e4620bda56b604bc3008d98752242fe693525d85af87786c5938acd60ba435b
-size 660
+oid sha256:104ad65f8d1aca613cb00702991ee0c4260cbf0e0c7fa6d1cbce01e88bd1efb3
+size 654
