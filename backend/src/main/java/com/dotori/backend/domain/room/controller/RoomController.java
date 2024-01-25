1f6bfac6552c7db93363fd6b928ef1fa0a002680
이하은 <hahi1108@gmail.com>
2024-01-25 21:21:50 +0900

    ãmod : 방 생ì세션 생성 및 커넥션 생성(방장) api mapping 수정
    Former-commit-id: bf2a4c0b113a835b3fc7fe6fd18af98c377b4fd3

--- backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02049140e20285d08e76d108ac9fa18676810aaeb3afb4b67eae00effcc4e035
-size 3249
+oid sha256:b0dc48a85652498c7dae6751e6f88f6cb131e42c0571b22659024ef987ec8580
+size 3016
