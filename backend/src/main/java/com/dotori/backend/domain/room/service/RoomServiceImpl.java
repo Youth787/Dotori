470f2ade36ec77d5d2816fa511c5e4442e074372
이하은 <hahi1108@gmail.com>
2024-01-25 16:29:37 +0900

    mod : 메서드 명 수정
    Former-commit-id: c782bf314404a7f43566ebdce237ce33c3ab0244

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6f109d5ca73ef6b2d6e6a8420dc9956e479cb237bb9f25812d155895f21ad79
-size 2651
+oid sha256:5fc05f10444ae2c2ee359a3ff1aae6f2d189f9039a7636abd6d7549fcfb5183d
+size 2644
