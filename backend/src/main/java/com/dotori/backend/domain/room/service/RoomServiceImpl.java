98ff16407cc091df39bf2d0ed0f9436cbbac9789
이하은 <hahi1108@gmail.com>
2024-01-25 16:19:42 +0900

    feat :  방과 ì»connection 생성 후 토큰 반환(방장)
    Former-commit-id: 3f2c9b7ea2b8278ffd3813bb5561edb217ac4401

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1940ef9b732221abb52d36ea806a00f6d17539d735ab99056d477db296ec57f
-size 1980
+oid sha256:b6f109d5ca73ef6b2d6e6a8420dc9956e479cb237bb9f25812d155895f21ad79
+size 2651
