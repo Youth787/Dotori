9df9ae136be796bba2671b3518fd5cc6e7c34f9e
jinooyang <jinooyang@gmail.com>
2024-01-25 14:08:20 +0900

    refactor : recording page, component 분리-> component/RecordingPageComponents
    Former-commit-id: 0c0fc2c9a63efa4d55468eb41fd009b87584a5eb

---
+++ Frontend/src/components/RecordingPageComponents/MainScene.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0f8b3eba5ed8ebd5029cc5624194316e282569b36fce10bca8337ae9a1b0019d
+size 2688
---
+++ Frontend/src/components/RecordingPageComponents/SideBar.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ea45f31b7e14112c97d726f935f21cf10bd782dd3f8ab1c8f6d3cc7a5f708e42
+size 3537
