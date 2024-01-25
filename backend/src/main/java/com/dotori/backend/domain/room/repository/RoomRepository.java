84af8423d43fefc5bdc74834629500ec61c23ae9
이하은 <hahi1108@gmail.com>
2024-01-26 01:33:10 +0900

    fix :  ãsession 생성 시 params null 오류 해결, api 테스트용 더미 코드 추가
    Former-commit-id: 773ac76085b24ce16d2da26ace225f5a6af24d99

--- backend/src/main/java/com/dotori/backend/domain/room/repository/RoomRepository.java
+++ backend/src/main/java/com/dotori/backend/domain/room/repository/RoomRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:104ad65f8d1aca613cb00702991ee0c4260cbf0e0c7fa6d1cbce01e88bd1efb3
-size 654
+oid sha256:7f1b186a10739fa588269fb2ee020abde4424ce58a78d38f0e8c48499fb23acd
+size 829
