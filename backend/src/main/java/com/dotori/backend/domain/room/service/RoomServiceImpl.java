2d12a31ef8eb65f84f8a82b391b60553558b02ec
이하은 <hahi1108@gmail.com>
2024-01-25 15:23:26 +0900

    feat : ë 방 생성 정보와 session id DB에 저장
    Former-commit-id: 9b79e12bf7cac36736413dd123384e25fb4bd6b4

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0512342f07e5791ed54268d1847eeb6cf4b0c2fed48c56a64b80a02f8d082e69
-size 1177
+oid sha256:e1940ef9b732221abb52d36ea806a00f6d17539d735ab99056d477db296ec57f
+size 1980
