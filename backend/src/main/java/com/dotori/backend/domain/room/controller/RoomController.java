c1a17925c8d02cb9431d2660bae9e3c5dc771ded
이하은 <hahi1108@gmail.com>
2024-01-25 14:26:13 +0900

    feat : 방ãëëë° 방(session) 생성
    Former-commit-id: 28c1c2908a02d35d8fd2745638cbee2e400aa45c

--- backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f56dc3d1958c3998f7c39f6dbfe0b6a888d6ca99b44c88f5c8b483a8428153e
-size 2864
+oid sha256:e826cd625f5bd863c554ef076843f7ca9eca5f934d2fd4a7a711a9794c996f0a
+size 3387
