2fb1c551823a2d4eda7540a89a7ff9a7f93f2e6c
yeonmi <wkdus885@naver.com>
2024-01-26 12:35:19 +0900

    feat : connection.getToken() 값 반환
    Former-commit-id: 61c490fbbbd3faffed6e58c84c1503202d7e70b7

--- backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbe85e7368315abdbe349300866b9625924780c13ba39bb6ac15e3a107361f94
-size 5030
+oid sha256:2326dbfbb730a3d25b6b4312d3654fde33577174cc70bb307acdc0c8b943bd99
+size 4458
