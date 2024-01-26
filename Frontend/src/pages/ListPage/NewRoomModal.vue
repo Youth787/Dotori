0ea84f23591f06581eafbb210ef15e8bac505957
jinooyang <jinooyang@gmail.com>
2024-01-26 17:33:04 +0900

    add : 이전 커밋에 누락된 내용 추가 (add .안하고 커밋했었음)
    Former-commit-id: a3a7f1e054c88306a8894fe45b5c47c8970c4723

--- Frontend/src/pages/ListPage/NewRoomModal.vue
+++ Frontend/src/pages/ListPage/NewRoomModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49068b000be87dad1c2e5af06ae868ab83a66e7b65aa4316d283c90e20eabd0c
-size 5453
+oid sha256:f26497ac79512f81a4c1093851da0c1aa49f22bae728fd77602401410ce6c6a2
+size 6051
---
+++ Frontend/src/stores/openvidu.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1d8c895f476aee98a8883119de9daeb95af0749d4940154fddde9922e522abb5
+size 1449
