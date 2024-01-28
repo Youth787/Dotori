c546b245ce64e761954fc99e7e2f748e3b5d38e0
jinooyang <jinooyang@gmail.com>
2024-01-28 21:17:09 +0900

    test : 화면넘어가도 오픈비두 연결되어있는지 확인하고 주석으로 코드 남김.recordingroom에서 채팅 보내는 주석 코드와 대기방의 채팅 전송 버튼 눌렀을때 모두 채팅 전송되는것을 확인함
    Former-commit-id: 077d5a2781509117a1564123d3ad0a896fcee2b7

--- Frontend/src/pages/RecordingPage/RecordingRoom.vue
+++ Frontend/src/pages/RecordingPage/RecordingRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:825e5b46d8cc46a3bd67425888c19cc43a026345d74b5886d70ff7c4cd465865
-size 3368
+oid sha256:210db1fa8dae52d45e0ee7e43a859b467c4f1fa1bc2ea39b57fb91e4f7d72856
+size 3676
