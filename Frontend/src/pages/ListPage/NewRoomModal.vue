4619e7cbcadb212e1a619b58267ac73d2206267e
이하은 <hahi1108@gmail.com>
2024-01-28 20:41:40 +0900

    feat : (front) 방 생성 및 방 token 받기 (방장용)
    Former-commit-id: 6f5f2dd26fd2a054d84b9e2cd0917800297270c5

--- Frontend/src/pages/ListPage/NewRoomModal.vue
+++ Frontend/src/pages/ListPage/NewRoomModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a09fa1b8e534441641784dafdfa3af30f33a0786de2e00d944c7cd0c736243d
-size 5290
+oid sha256:d60b56895a213c0ca981e22a8ff35e8e875c3a322af64fc076d09e19474ba4e0
+size 5548
