d2ac4ea607b4314b1a8a6491dd4e60e8cbb3b97a
jinooyang <jinooyang@gmail.com>
2024-01-28 19:20:31 +0900

    feat : 대기방에 인원수에 따라 플레이어 카드 다르게 찍히는 기능
    Former-commit-id: 42e3af3d6b81fa05210fc11660525cdeaf8a9294

--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9d4da8cbf6563955169f8651ca2e5ec006d789713ce24b5e4367b0f6e13d533
+oid sha256:4192adb75953382553ff9f4e64ee1cbe8d256e696fd53d9126b3e001120c95bc
 size 2077
