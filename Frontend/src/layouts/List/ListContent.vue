3d4adb30981708aa99ec369cebed8e09324be181
bufflect <bndgc@naver.com>
2024-01-28 23:10:32 +0900

    add: video-saved-path
    Former-commit-id: ca61044cfae1dd0121436d82059d92863f7ca1b2

--- Frontend/src/layouts/List/ListContent.vue
+++ Frontend/src/layouts/List/ListContent.vue
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1757e271b915deface41079998600b446decac48cfe94c277781784bf92b71f9
-size 150
+<template>
+  <q-page-container>
+    <router-view/>
+  </q-page-container>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
