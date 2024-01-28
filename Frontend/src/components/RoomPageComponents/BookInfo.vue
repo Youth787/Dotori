1df30fe570d27af1c21e649347d3dc2e6d6426fe
jinooyang <jinooyang@gmail.com>
2024-01-28 19:39:00 +0900

    feat : 플레이어 이름 다르게 표기, 프로필 이미지 다르게 표기, 이미지 스케일링 수정
    Former-commit-id: 6d6d2aaae370f63040e9e93439990c01a70e5b4c

--- Frontend/src/components/RoomPageComponents/BookInfo.vue
+++ Frontend/src/components/RoomPageComponents/BookInfo.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a4f54429b5852bf8e2fa5f6ef58e5606e4ba2675095c30ff3e429a67dca9100
-size 1275
+oid sha256:f1bfee612dc9237b907251c9b29ee82139c253beb3cdbe15173b7e4407099deb
+size 1180
