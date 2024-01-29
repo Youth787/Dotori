e08391491a4a418b5969f9efd1d42665a9f41571
jinooyang <jinooyang@gmail.com>
2024-01-29 18:00:02 +0900

    fixing : can't add subscriber to subscribers
    Former-commit-id: 7089064bf166b4b5c1de62ccb3db41be276a2f81

--- Frontend/src/webrtctest/WebRTCTestPage.vue
+++ Frontend/src/webrtctest/WebRTCTestPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24c7493306b282de1f92af3715a5bb3c2c32b1415504951623dd893291db1d41
-size 4727
+oid sha256:ddf6dcf53435cbf701a2dd967bf2e2093c41d0dbd9b2f26b4c6feccd21900970
+size 4788
