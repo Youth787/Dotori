53113020d2f8c02caefebd595aeb07df3073ddb8
이하은 <hahi1108@gmail.com>
2024-01-29 23:45:26 +0900

    Feat : 방 참여 시, 참여 가능 관련 유효성 검사 및 방 참여 인원 갱신(추가) 메서드
    Former-commit-id: 2e8d4eaac710d09c467456ec177f1ef5adf869d3

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed804174bdc128ac72a2f061e77d8281d28829d8974272ccb80517b96d8b668b
-size 4762
+oid sha256:c178eb77a5794afc6772c6a353efe0a23c2de6983c1a4d4375f0f00f84247a6d
+size 6085
