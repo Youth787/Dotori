501fb3fd861a7d5b95baa73179fdf1d593bf2e7f
문준형 <moonabcd@naver.com>
2024-01-29 17:35:33 +0900

    fix : 접근제한 모두해제
    Former-commit-id: 0aecb4a32cb6e9cf73aaa5aeeb386aa63d8234f6

--- backend/src/main/java/com/dotori/backend/domain/member/config/SecurityConfig.java
+++ backend/src/main/java/com/dotori/backend/domain/member/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ea71fb90897cb0a6fa1518a1f8590fab11c39b033241378e07d98371d6a522e
-size 4989
+oid sha256:6fdae6b302fc85d2bf93f8d433694907d078b3d222d583160ad72e293d86deb8
+size 4995
