b1f195b656b7f214da8ba14d7030821f74e19deb
이하은 <hahi1108@gmail.com>
2024-01-29 23:44:48 +0900

    Feat : 방 참여 시 connection 생성 메서드 작성 (일반 유저)
    Former-commit-id: bc3809bdeafed8b9cdc7b2d6731797038ee3fc7e

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efa6a1bbf294d481ba5ceff4d70e9ff079747d1bd1aeacbff3d98df0504cbe4e
-size 4486
+oid sha256:ed804174bdc128ac72a2f061e77d8281d28829d8974272ccb80517b96d8b668b
+size 4762
