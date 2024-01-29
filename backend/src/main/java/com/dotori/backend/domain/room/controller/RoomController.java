34b231abbc149c637dab99165e438fc37814d6e7
이하은 <hahi1108@gmail.com>
2024-01-29 23:52:07 +0900

    feat : 방 참여 api (이게 찐)
    Former-commit-id: a83de5f2415a052036c9e3a1fcf7b45b1d65efca

--- backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82ac104fd578ed06cc8ac2e52966879ac4164b84bcdc7e9ea984de6180d6600b
-size 4668
+oid sha256:30ff6c3d06e14a59c64b758f58399150b462e7c36aa0b20e8c75c8a4b17bdc73
+size 4387
