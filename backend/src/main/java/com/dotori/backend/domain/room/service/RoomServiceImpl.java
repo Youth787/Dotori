12a8df04a9a45feb6a7f5361851b23c62a553251
이하은 <hahi1108@gmail.com>
2024-01-29 23:46:23 +0900

    Feat : 방 나가기 시, 방 참여 인원 갱신(제거) 및 방 삭제 유효성 검사를 통한 삭제 메서드 작성
    Former-commit-id: ad5ddccd990f671fcd8919013fb0923269a017f6

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c178eb77a5794afc6772c6a353efe0a23c2de6983c1a4d4375f0f00f84247a6d
-size 6085
+oid sha256:91a5e0fa5f2929e6b483606c5e9a5271c55c3f1774947efbf7bde410ac94ae37
+size 6762
