f1492bb7ab1e25b1b51e8eba95464995a260d0b0
jinooyang <jinooyang@gmail.com>
2024-01-29 17:49:50 +0900

    add : 다른 사람 비디오 나오는지 테스트
    Former-commit-id: b584cc4a536bb4d7b39e99e5e252efc0e3f598bf

--- Frontend/src/webrtctest/WebRTCTestPage.vue
+++ Frontend/src/webrtctest/WebRTCTestPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b53d55b9fb030b1078cc490973ed8b1dd4b3401a6cb5184f22b816fbba77e5c
-size 4409
+oid sha256:24c7493306b282de1f92af3715a5bb3c2c32b1415504951623dd893291db1d41
+size 4727
