6f6e2b0d16dcf956cb9283e8fecb2363e8ec6761
jinooyang <jinooyang@gmail.com>
2024-01-29 20:48:27 +0900

    fix:added subscribeId
    Former-commit-id: e864c05b97269f1ef6fde5c49fadaed30d255988

--- Frontend/src/webrtctest/WebRTCTestPage.vue
+++ Frontend/src/webrtctest/WebRTCTestPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddf6dcf53435cbf701a2dd967bf2e2093c41d0dbd9b2f26b4c6feccd21900970
-size 4788
+oid sha256:d326372c72f79c97b2eb1abed41a73ba694b997b567b38896946e341f1d74e97
+size 4844
