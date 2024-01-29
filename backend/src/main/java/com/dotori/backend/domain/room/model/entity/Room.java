ddc130ebc2e22bb25ab797333299f9594c067bfe
이하은 <hahi1108@gmail.com>
2024-01-29 23:48:23 +0900

    mod : 방 참여 인원 갱신을 위해 Room 엔티티 joinCnt setter 허용
    Former-commit-id: 0b4d02a77270b507a5a59bc966d726e44323ab63

--- backend/src/main/java/com/dotori/backend/domain/room/model/entity/Room.java
+++ backend/src/main/java/com/dotori/backend/domain/room/model/entity/Room.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f765bed471644b1e346c2b9f4593783ef3586185ceee80ce8f02d105bfa1f71c
-size 2003
+oid sha256:0aea31361d128aac72953b45df589d590a8270d5f88e7851c905e898386a80c7
+size 2025
