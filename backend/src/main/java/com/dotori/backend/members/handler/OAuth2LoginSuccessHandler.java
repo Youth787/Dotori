7a5a08237f248f3ff2bf369f9e48d14194894812
문준형 <moonabcd@naver.com>
2024-01-29 14:15:12 +0900

    fix : 코드정리
    Former-commit-id: 2834fdeb6cc3e3d4a182dc135417555f8ce4c086

--- backend/src/main/java/com/dotori/backend/members/handler/OAuth2LoginSuccessHandler.java
+++ backend/src/main/java/com/dotori/backend/members/handler/OAuth2LoginSuccessHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:175a97bfb70a90ef8f50728279c7269285b259763b1a8db35b31369de7fbaad5
-size 2750
+oid sha256:6c087b8345de8e1ce4d25a50c9148927596f3be370de4ed4a24835b75d363338
+size 2763
