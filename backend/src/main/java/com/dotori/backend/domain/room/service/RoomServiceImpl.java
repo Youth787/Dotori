1a03c0101ef5e2104e577223488784a2ab3894b5
이하은 <hahi1108@gmail.com>
2024-01-29 23:43:58 +0900

    Feat : 방 관련 조회 서비스 메서드 작성
    Former-commit-id: ec9caa23302d21d72f01c60cdc1f7ebe5d11a1f7

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4ba77349f41b4a06361bd479c60f8485b680bba5474b9c5f9e973fecb070af5
-size 5149
+oid sha256:efa6a1bbf294d481ba5ceff4d70e9ff079747d1bd1aeacbff3d98df0504cbe4e
+size 4486
