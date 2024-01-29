998b7323e464fa114fd226c293d1cbfcd6c0a750
이하은 <hahi1108@gmail.com>
2024-01-29 23:51:09 +0900

    comment : 불필요한 주석 삭제
    Former-commit-id: 773587bbe3bd49e1ac7719e69f4ab968cc8520a2

--- backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2c80d0af945de8c4f4b26fbdea76d55b93c5e9bc0f3af166de45741d91988cc
-size 4082
+oid sha256:27ff5551886f323d710cf81b5829483d06bba566b0aa2efe1e3cbc3b1045e559
+size 4036
