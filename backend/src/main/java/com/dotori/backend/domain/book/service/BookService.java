2f7699c08bea455759d64013c2f6273df4c59ec5
SeokHyun Cho <doongdang@naver.com>
2024-01-29 09:51:00 +0900

    update : 책 정보 조회 API 설계에 맞게 수정
    Former-commit-id: 5680871ea32333064e65fe9e901e6faabf7bd895

--- backend/src/main/java/com/dotori/backend/domain/book/service/BookService.java
+++ backend/src/main/java/com/dotori/backend/domain/book/service/BookService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6e6184a63962300bec90928073e481f2393e668c7218e6c23f4bdf0553d40f2
-size 1139
+oid sha256:cffd004a57bfa8704b481f9b11f3205e98a145c4f30b8de4a3a3125e38aa7bef
+size 1753
