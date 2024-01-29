62b3ca84a5770254aeb84fa4f972ecf0bd35fdf2
문준형 <moonabcd@naver.com>
2024-01-29 15:01:20 +0900

    fix : member -> memberTemp로 임시변경
    Former-commit-id: 59ea416e17699c92c94b2fee3b785f404ea55c5e

--- backend/src/main/java/com/dotori/backend/domain/member/service/CustomOAuth2UserService.java
+++ backend/src/main/java/com/dotori/backend/domain/member/service/CustomOAuth2UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94a303c9ee60fd90f722688f82ad24ae569e2ef4bf68c1eadb1f30af91c675ce
-size 4722
+oid sha256:bbf7f0b1f6b4d3c3d4ee168efd68c75379e81bef7bf66706c253b2d3d56cac00
+size 4746
