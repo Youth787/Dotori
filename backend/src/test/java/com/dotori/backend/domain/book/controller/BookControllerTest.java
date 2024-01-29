eb64b13adb669f89ddc5a6695ca0949ace0a706f
SeokHyun Cho <doongdang@naver.com>
2024-01-29 09:53:05 +0900

    test : 책 정보 조회 책 정보 조회 API의 변경에 따른 테스트
    Former-commit-id: c04e1da7d7f33234459aaa267cf0210c9f0e688e

--- backend/src/test/java/com/dotori/backend/domain/book/controller/BookControllerTest.java
+++ backend/src/test/java/com/dotori/backend/domain/book/controller/BookControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5bbba7c4707bd993aa01252c1d4a8de57b4d63602628e27331795716ea76e81a
-size 2132
+oid sha256:ca1c53d4b0f536b3fca0dc3cc5fe752213f237274dfac5f455436597fbdf60ca
+size 2661
