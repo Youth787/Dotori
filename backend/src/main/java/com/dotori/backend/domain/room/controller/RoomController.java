84da12fee3826bd2bdb72710ce57b9250ed68cd9
이하은 <hahi1108@gmail.com>
2024-01-29 23:50:47 +0900

    feat : 방 나가기 api
    Former-commit-id: 3b4b963c88bac7599dde007b09c5a73e2c099284

--- backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8236a5ce6f164d7850245f7c9adf4d792684f82bfdd7fd14b738b1509e4e0d3e
-size 4597
+oid sha256:c2c80d0af945de8c4f4b26fbdea76d55b93c5e9bc0f3af166de45741d91988cc
+size 4082
