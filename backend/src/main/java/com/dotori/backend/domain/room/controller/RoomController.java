9d9ccdf84110039a8cf33f5996706b875a141cb8
이하은 <hahi1108@gmail.com>
2024-01-30 00:44:45 +0900

    mod : 방에 멤버 추가 전 openvidu 서버와 세션 동기화
    Former-commit-id: 24c3b9f309814309b68f4e78df9cd6d2b982af46

--- backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bf1e855d411e85599b15c17214f95154b37427b6941f2ee747a5a135a3e1d8b
-size 4958
+oid sha256:965b02b7117a56870953a52d9402a4a2c5156b3fef7e9e092d2eebf815c1da95
+size 4979
