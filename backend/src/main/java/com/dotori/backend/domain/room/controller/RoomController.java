b4dcc720de04863e10c3b06488d958372de22fd8
이하은 <hahi1108@gmail.com>
2024-01-30 02:17:11 +0900

    fix : 방 참여 시 제한 인원 이상 참여 시도 시 유효성 검사를 통해 cut
    Former-commit-id: a311a0cf03a8447669f85be13098631e28e8bd9d

--- backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:965b02b7117a56870953a52d9402a4a2c5156b3fef7e9e092d2eebf815c1da95
-size 4979
+oid sha256:fb8c2f810c46725739af152a4f2721c5ff363544b74a24ea6ba86bccc075748e
+size 5187
