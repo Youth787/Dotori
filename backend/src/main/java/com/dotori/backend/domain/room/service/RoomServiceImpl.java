92c62f3aa70bb10a39ad1d3aeeaff52a2e900472
이하은 <hahi1108@gmail.com>
2024-01-29 12:01:57 +0900

    Refactor : 불필요한  코드 삭제
    Former-commit-id: c5e4c359817b31ab200d2d8e086c521868cfdd33

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a319fddedff814b1d55e43e58bb47a686194e0ae64ed50bd6d285e41ecb8a93
-size 4162
+oid sha256:bffe5db49c54b19e6d08dc33e34f880bf67c8b35fba84ca0938f74e70218c01e
+size 4108
