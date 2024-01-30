8e511b34d1d93223711582a51f765e374dd351c9
jh <bndgc@naver.com>
2024-01-30 14:48:53 +0900

    feat: get-scene-api   - scene with roles   - scene with scripts
    Former-commit-id: 79b1e62e98baba57bd361a696695be82c8f75a73

--- backend/src/main/java/com/dotori/backend/domain/book/controller/BookController.java
+++ backend/src/main/java/com/dotori/backend/domain/book/controller/BookController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb9975257a1fbc0e45fd5bf25e28de0888ceda32763a9579b22eceab0104bd25
-size 1487
+oid sha256:d6758e04866a55778a62838b9d7ad0315e6c8545b8cbeec064051d2975f32cef
+size 1782
