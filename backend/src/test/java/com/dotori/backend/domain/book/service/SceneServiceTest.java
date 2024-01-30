05365e3734a8d9bb09536817933e924e79e54c25
bufflect <bndgc@naver.com>
2024-01-30 21:54:10 +0900

    test: SceneServiceTest
    Former-commit-id: be208e48185cddb8d57b30a251a72b3e1279f398

--- backend/src/test/java/com/dotori/backend/domain/book/service/SceneServiceTest.java
+++ backend/src/test/java/com/dotori/backend/domain/book/service/SceneServiceTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0dc3ebd00293886d914828a49d501f96bd283a48505c879b86bd855cdc78785
-size 1964
+oid sha256:3cc4fec93662deda5bebeb018a7f5282a459677aeb5e9918ceed5d0b35b28b76
+size 3660
