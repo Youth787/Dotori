8a219405b4c48cbc758ed87284254ceba5af52be
bufflect <bndgc@naver.com>
2024-01-30 21:53:51 +0900

    test: SceneRepositoryTest
    Former-commit-id: a7788cfaa01d0652013bd5da4a9bdad4f64d91d9

--- backend/src/test/java/com/dotori/backend/domain/book/repository/SceneRepositoryTest.java
+++ backend/src/test/java/com/dotori/backend/domain/book/repository/SceneRepositoryTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19592aa76e27cddf2f77aa6cc96c28108a9d697dcea14c7d428161baa8bd646e
-size 2007
+oid sha256:af13941142c5d448558495c0368ccedc80cca169889f3fc104fc91a9ae828849
+size 3692
