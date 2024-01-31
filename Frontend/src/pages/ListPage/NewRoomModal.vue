0dd2f6a5d126db5971663c00dd36104f0f0473a6
이하은 <hahi1108@gmail.com>
2024-02-01 01:37:48 +0900

    fix : 방 정보 입력 방식 반응형 변수 도입 및 실제 입력 값 대입
    Former-commit-id: 3047d3b269bb83230b280d92c9b2aa5953efc51a

--- Frontend/src/pages/ListPage/NewRoomModal.vue
+++ Frontend/src/pages/ListPage/NewRoomModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ad1d3db0ae34ea351584ac37d7236abeecde08cd4285a91da3465bb9ce5ffab
-size 5350
+oid sha256:fcdec0f0d58f3c67d6b27c0993fdb9957a8ffbbfc1e4cc3a5e000b9ec6872364
+size 5407
