a5149cdd6239d4d72207cbdc1ca07144761e50bd
이하은 <hahi1108@gmail.com>
2024-01-31 16:02:52 +0900

    feat : ë: 비동기 요청 처리
    Former-commit-id: e74d4b89c90ee8a02dae0d11658f7c6b2627fbac

--- Frontend/src/pages/ListPage/NewRoomModal.vue
+++ Frontend/src/pages/ListPage/NewRoomModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91beeb05ab4e4f7342c2993c848de61578b7a4df54cc4121743d6a52f005138d
-size 6281
+oid sha256:fd0ced7f0e797504b1e5b52547c9890cf759b6d261c648cc21e9f86c74dc82a9
+size 6407
