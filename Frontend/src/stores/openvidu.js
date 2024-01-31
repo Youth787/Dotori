fac018a3af1e8cfb58fa285419d3f146339380e4
이하은 <hahi1108@gmail.com>
2024-02-01 01:06:54 +0900

    feat : 방 생성 시 책 정보도 같이 axios 전송
    Former-commit-id: bf6e7af9f405d97d64ee0117853ce46bacb1d652

--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47b10507dc6c7dd5fcc60a17e84db5a6cb4763a67024a9996247f9e99a11645c
-size 5318
+oid sha256:3070589172de39c95ef6cb16507f5bfc8b3f069f87e823215a6124c4e470cee6
+size 5471
