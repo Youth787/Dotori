a71f4468934a19d281637ddfd30d738b4fb17e5d
이하은 <hahi1108@gmail.com>
2024-01-31 15:54:08 +0900

    refactor : 방 공개/비공개 토글 체크박스 변수명 수정
    Former-commit-id: fbf0cb4989106f51a1bad269c367beb9aedbcb05

--- Frontend/src/pages/ListPage/NewRoomModal.vue
+++ Frontend/src/pages/ListPage/NewRoomModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1186d5900af27ba53ba1fc76336abea99dddb1211e86291e2fe5cb100fdd3fe7
-size 6424
+oid sha256:91beeb05ab4e4f7342c2993c848de61578b7a4df54cc4121743d6a52f005138d
+size 6281
