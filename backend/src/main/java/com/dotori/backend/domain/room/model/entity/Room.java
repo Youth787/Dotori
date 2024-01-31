75ba506fbf6c4889b9b8d31ba8752f9de476eaa1
이하은 <hahi1108@gmail.com>
2024-01-31 11:01:10 +0900

    mod : ãRoom 엔티티 빌더 패턴 변경
    Former-commit-id: d96da008788dcc2809d7cd566cc84073366aa637

--- backend/src/main/java/com/dotori/backend/domain/room/model/entity/Room.java
+++ backend/src/main/java/com/dotori/backend/domain/room/model/entity/Room.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34ac56d6c1f0787ee1400f133068ade64ac1c53ec362e62d09446156a007eb5e
-size 2124
+oid sha256:ed119fe8b41dd93e4303dcdc01ab601de2236b5708a6816b2db18f85ccafba29
+size 2179
