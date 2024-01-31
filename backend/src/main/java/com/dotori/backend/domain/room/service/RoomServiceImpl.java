132d0882fc9169bc3bd38306bc15500897929c88
이하은 <hahi1108@gmail.com>
2024-01-31 11:04:11 +0900

    comment : Room 엔티티 생성 주석 코드 추가
    Former-commit-id: 3e99e683a623dcea9bb368bd3c85ed92e9e3e3cd

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:989bf72020aff217bf69dddf5fc5ba155bd35d8c987a28163d9a736043bd34a7
-size 7444
+oid sha256:e947129f14108d52f82da006e4f36ff85dc2a8d00345a40f9e3d72cc88234906
+size 7603
