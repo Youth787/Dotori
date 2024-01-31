889c1aa5b3dfc9d118fe62c265e2b33144b11819
이하은 <hahi1108@gmail.com>
2024-02-01 01:06:54 +0900

    feat : 방 생성 시 책 정보도 같이 axios 전송
    Former-commit-id: bf6e7af9f405d97d64ee0117853ce46bacb1d652

--- Frontend/src/pages/ListPage/NewRoomModal.vue
+++ Frontend/src/pages/ListPage/NewRoomModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e03c087e47a191ab02de4563e9e6f7acd9c97959c8fe2ae7c0894c45e31f211
-size 5262
+oid sha256:9ad1d3db0ae34ea351584ac37d7236abeecde08cd4285a91da3465bb9ce5ffab
+size 5350
