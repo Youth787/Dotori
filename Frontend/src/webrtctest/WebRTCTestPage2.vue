cc1d672bb9c613502a3f44c45d42a2bf60e458e1
jinooyang <jinooyang@gmail.com>
2024-01-31 17:52:05 +0900

    test audio success
    Former-commit-id: d089053d27effd756b862cdb64f8825c63cff773

--- Frontend/src/webrtctest/WebRTCTestPage2.vue
+++ Frontend/src/webrtctest/WebRTCTestPage2.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3206c36b1ac92acda2f758873a00df4fffc20a2a17dea50027c2a630a5d907fb
-size 17405
+oid sha256:1cd78d9b8db4cff89cc88af206ba8947e1d06ada6e40684ed0c44bcf4ea1dfdf
+size 17419
