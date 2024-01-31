1c4e24f8b44060114da8b01c2ebd2e589bb81d33
이하은 <hahi1108@gmail.com>
2024-01-31 22:35:22 +0900

    백 잘못건드림..죄송..
    Former-commit-id: c88623dfa6da5662c5f84082dca1318d93fdc1ea

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef4fefcc76f07a6083cb0ccfc2e4499fda9276e3b9368c1f1cd3f22d6d0eff7f
-size 3595
+oid sha256:a698ca55e72b9d5ce03d6c5e44f85f966eff6c396a74e7f628b92693454de2f9
+size 3596
