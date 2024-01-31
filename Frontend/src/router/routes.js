55102239b93e6559c5d566b0cf155698a829c6cf
jinooyang <jinooyang@gmail.com>
2024-01-31 10:26:17 +0900

    my video test
    Former-commit-id: 92e772807ffb4fe14fc23dfa94c918efe3fe1780

--- Frontend/src/router/routes.js
+++ Frontend/src/router/routes.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3e874687f8f0a32fa72d6a99a4cb4db6b19eeaa23ed6fc0d85c12d3f5914809
-size 1960
+oid sha256:50ebb12f859caded173ef99229de55bc5f71eb8cb2562172a52347b2a9f2278d
+size 2055
---
+++ Frontend/src/webrtctest/WebRTCTestPage2.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:07fcc877b0a4e4da6b05f5947910487a1d75db07a50b94aa10af2c0844f19649
+size 17545
