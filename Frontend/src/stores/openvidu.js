e8f1354df0eedf0b30c1d051bfe5f6595f697811
이하은 <hahi1108@gmail.com>
2024-01-31 10:29:06 +0900

    mod : 방 생성 시, token 반환까지 한번에 이루어지도록 변경
    Former-commit-id: 20fc2aeed962b2115dc6069a2ea3be7f83fa9369

--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95907b210088e28daaac8b0d3fd4529aff9308438630423c999f77bb9b6ab497
-size 3955
+oid sha256:4ecee760d7769a824b09ce07ad5feea26557364b914f9dbca64398fe8085ed73
+size 4195
