5878e8fb317b1141ab0f70b5fb339e37a94ae183
이하은 <hahi1108@gmail.com>
2024-01-31 10:30:48 +0900

    feat : 방 참여 인원 정보 갱신 axios 요청 메서드 작성
    Former-commit-id: 801b586a8e7ce13f7606027059cca57f04051ff4

--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54d860f873b8270e175a87e7751c5194ae5d1c1065acac26fd68bb51a8b8e571
-size 4293
+oid sha256:16c739985484ea480e7a022dfc51833cccdf013038475ddefb1dfb3200730e47
+size 6231
