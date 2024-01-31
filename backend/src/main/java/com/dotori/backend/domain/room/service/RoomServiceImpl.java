92e84cbdadb62c58ce01443a0fe3cbae7c64925b
이하은 <hahi1108@gmail.com>
2024-01-31 17:27:00 +0900

    mod : book 컨트롤러 동작 시, book id로 book을 조회하도록 한다
    Former-commit-id: a7ef6f48fda23e7876276e0e2251ac0a495ed762

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e947129f14108d52f82da006e4f36ff85dc2a8d00345a40f9e3d72cc88234906
-size 7603
+oid sha256:70bc6759edef2552b1a67c77f692b1f74fc2929df02fe4d72db532418ed2d3e1
+size 7617
