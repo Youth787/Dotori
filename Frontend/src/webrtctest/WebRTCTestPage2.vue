1e8085af6fadab472d5a4f4bfdb2d598a1a8da9a
SeokHyun Cho <doongdang@naver.com>
2024-01-31 15:43:01 +0900

    test : 화상 3D캐릭터 공유까지 성공
    Former-commit-id: d7570ce338657b0c8a3343aa056ac37e1e573021

--- Frontend/src/webrtctest/WebRTCTestPage2.vue
+++ Frontend/src/webrtctest/WebRTCTestPage2.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07fcc877b0a4e4da6b05f5947910487a1d75db07a50b94aa10af2c0844f19649
-size 17545
+oid sha256:3206c36b1ac92acda2f758873a00df4fffc20a2a17dea50027c2a630a5d907fb
+size 17405
