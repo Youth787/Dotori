983886f875b3ba09987d5d04d091c99fc424749b
이하은 <hahi1108@gmail.com>
2024-02-01 23:28:08 +0900

    fix : 방 생성 api 관련 book dto, room dto 생성자 추가, openvidu 서버 url 수정, 방 생성 시 제한 인원 로직 수정
    Former-commit-id: c204e97c17963e3e6ce0fe63281d0a2c58f1de74

--- backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:776eb36eef3835922d9ae7b71ab86b6f52ea473457eb8415b480502d1b28fe6b
-size 5187
+oid sha256:c40428e6e8fd0cf157108ea2b553a41fdfde4f56ce44836574ef97ed9133a21e
+size 5212
