46d67f2fd9c9895954e5dfb4401ff1403e34fffb
이하은 <hahi1108@gmail.com>
2024-02-01 09:50:18 +0900

    merge
    Former-commit-id: 40790e2f945a48806fff046e08258eaf97984f82

--- backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:229db5b25ecef3a37b3d6e3bfc36eeacbd12df522407de45bc7f234b2218be95
-size 3145
+oid sha256:cbe85e7368315abdbe349300866b9625924780c13ba39bb6ac15e3a107361f94
+size 5030
--- backend/src/main/java/com/dotori/backend/domain/room/repository/RoomRepository.java
+++ backend/src/main/java/com/dotori/backend/domain/room/repository/RoomRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f1b186a10739fa588269fb2ee020abde4424ce58a78d38f0e8c48499fb23acd
-size 829
+oid sha256:2997ea427c52b1da1d401910373b691da0b91b6cceb8de8cd430274381ef3172
+size 472
