cc43f09feee946d91e6e22cc87ac4f6034da0348
문준형 <moonabcd@naver.com>
2024-02-01 10:09:26 +0900

    rebase: 1
    Former-commit-id: a123ba796450a56a60d9776674d10d46ef362345

--- backend/src/main/java/com/dotori/backend/members/dto/SocialMemberResponse.java
+++ backend/src/main/java/com/dotori/backend/members/dto/SocialMemberResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83c22e82790420da7e0b0a254e36b89ea5c4b4fd3ed6f1e70cbf3ae61c53bb90
-size 240
+oid sha256:5060dc77262bf2eb7977cc83a3b1b49ea10af253dc4bd9d69cc9a25a15efa1a6
+size 384
--- backend/src/main/java/com/dotori/backend/members/model/Member.java
+++ backend/src/main/java/com/dotori/backend/members/model/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33a92a8159a4900f3192bb08039f6e7a52f38f6d00a56d8d0b533aee355cde4a
-size 761
+oid sha256:db4a2c1eac5de59ce219813644549a58d8f8fd49bfc003d34bf365cd643d96a3
+size 961
