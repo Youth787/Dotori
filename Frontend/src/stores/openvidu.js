b35b1b600ee866daf60434f9b1f9189db26a1755
이하은 <hahi1108@gmail.com>
2024-02-01 15:38:54 +0900

    fix : 방 생성 정보 주입 방식 변경
    Former-commit-id: 8a9d57c95fc6a87395036f324082f8d0042bf85f

--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c295eea17247102258a79a1cc80116b77dbcf9852dd8a9d70d6186ef881dcea8
-size 5635
+oid sha256:5e96b0021e9d56a4c1066dea46a29d53ca65abf0685d0b37e19e2cea80469b26
+size 5615
