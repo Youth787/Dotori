ed9550982b991e882dc59c18000c43e782a1024e
이하은 <hahi1108@gmail.com>
2024-02-01 09:44:49 +0900

    mod : 테스트 ì 입력 삭제
    Former-commit-id: 119ce8f791dffedf5dddb0634ae64b400d8e16fe

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7207c8ccb6d53600b02e36634ec94dc901f946051eb07a560d6032d540c0421
-size 7376
+oid sha256:83661fd34c29e7b40bf7d5c96447d71f7ed19b57432acb0cf05972633fe07da4
+size 6260
