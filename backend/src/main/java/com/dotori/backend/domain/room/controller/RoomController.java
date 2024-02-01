619f0fa09e1dc99ad22e6a63bd7a50468e9e95db
이하은 <hahi1108@gmail.com>
2024-02-02 00:38:04 +0900

    feat : requestParam -> pathVariable로 수정
    Former-commit-id: 868d552bbb43219268640c49bfc1c70ab743420b

--- backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fec5f6769ee4c687ac4a68d88baa6b22eb08faa4ab50e2f46d847a024792fdf1
-size 5273
+oid sha256:5fb0cab39215d9f35ce049482e4bf8d06a3f2bf3f4f03615b8e5657e2957c57b
+size 5322
