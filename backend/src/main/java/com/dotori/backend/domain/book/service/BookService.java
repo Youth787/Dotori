9113d1371598b011c00b20ec29e8b2e38ee9aa46
SeokHyun Cho <doongdang@naver.com>
2024-02-01 09:08:04 +0900

    fix : 책 정보 조회 시 검색 결과 없음 처리
    Former-commit-id: e72ff13f4315cfc0882771ddb70cedff21b8012d

--- backend/src/main/java/com/dotori/backend/domain/book/service/BookService.java
+++ backend/src/main/java/com/dotori/backend/domain/book/service/BookService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c212dedc0f9fe529213d4a0ca1baa5043a0823f6319ead35cb2160594d5cc0f3
-size 1750
+oid sha256:f309236bb2d89b728e0513f8dfa19c47f770b2a5f3888046c9e14db2735d1597
+size 1782
