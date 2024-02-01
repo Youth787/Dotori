435ccacb13c00b6078c17be037413abd006b2728
이하은 <hahi1108@gmail.com>
2024-02-02 00:34:37 +0900

    comment : 불필요한 주석 및 확인용 코드 삭제
    Former-commit-id: 0affd98365c7a28c2b6c746384c91de91f69ae9d

--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc2f66897db3596ccb5144fdb5648fa111652741e5d4defac97e781a0b944266
-size 5510
+oid sha256:fee6d1876c5bf6d5601ff5b453b68968746464640fa4b2e0d092892f70b3ed1d
+size 5219
