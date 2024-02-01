072874b7529b23b2d1f39d0b9a11a76581fc0204
jinooyang <jinooyang@gmail.com>
2024-02-01 18:02:02 +0900

    test : setting 3dmodel testing env
    Former-commit-id: d093f6879a8c28306b005be84dc4965e4a0343e8

---
+++ Frontend/src/components/RecordingPageComponents/OvVideo.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8b78192e017a65ae4f71585e2e6dbaf1d88ccc20c99940e7ba20b20ff1125f27
+size 408
---
+++ Frontend/src/stores/openvidu2.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f365cc02ce21869b93076ac91632d01e100edab674bf83b361faf8f2ae27952b
+size 3185
