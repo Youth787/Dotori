f4b89ddf52d0761df69a0b36c52ae8890e445ab0
이하은 <hahi1108@gmail.com>
2024-02-01 10:13:47 +0900

    fix : 방 생성 기능에 Book 관련 코드 수정 (Book 기능 반영)
    Former-commit-id: 40003dfc97203ca1e6186c9facaf649cc989638c

--- backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0adac55cfec03794472d752006b9746bb9622dc38f98afbe73bae90f30df3793
+oid sha256:dada579964b20e691907deb7607fb965d55aa486392b2809f13f034eec2f86da
 size 5188
--- backend/src/main/java/com/dotori/backend/domain/room/model/dto/RoomInitializationDto.java
+++ backend/src/main/java/com/dotori/backend/domain/room/model/dto/RoomInitializationDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b392d545a78a4621a9eb9ccc92babd528b9963a1e042fbdf359eab4e2ab2f7d3
-size 406
+oid sha256:4c22fb0c348e6f3ef211aacac2f7da8173cd44fa768ef41d69076c9d96f49f0c
+size 461
