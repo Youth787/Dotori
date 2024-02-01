294157162c75c8b878033599f182282e4e37bf0a
이하은 <hahi1108@gmail.com>
2024-02-01 11:54:16 +0900

    fix : rebase 오류
    Former-commit-id: ef278d56da0b8e0f39c1b993c884b906fe32cc42

--- backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dada579964b20e691907deb7607fb965d55aa486392b2809f13f034eec2f86da
-size 5188
+oid sha256:776eb36eef3835922d9ae7b71ab86b6f52ea473457eb8415b480502d1b28fe6b
+size 5187
