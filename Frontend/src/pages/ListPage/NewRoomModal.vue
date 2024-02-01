01821d568adf9e40b533bd3761e32a779743c845
이하은 <hahi1108@gmail.com>
2024-02-01 15:38:54 +0900

    fix : 방 생성 정보 주입 방식 변경
    Former-commit-id: 8a9d57c95fc6a87395036f324082f8d0042bf85f

--- Frontend/src/pages/ListPage/NewRoomModal.vue
+++ Frontend/src/pages/ListPage/NewRoomModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13f0bdd597a281b6636c2d15fd53bbf6fdd968699c02eedfb25f7b65169f21a9
-size 6690
+oid sha256:16ab3b7ef58e46e2e952079f7de08a4d0880e85fc3220062e98500cc2edfcaaf
+size 6732
