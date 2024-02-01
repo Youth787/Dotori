39671f94a923bc64461b11a31162b3a1b57f872f
SeokHyun Cho <doongdang@naver.com>
2024-02-01 09:09:29 +0900

    update : stream
    Former-commit-id: 659f5b92b47c472c5df37f5e78e82a9663a98d5d

--- backend/src/main/java/com/dotori/backend/domain/book/service/BookService.java
+++ backend/src/main/java/com/dotori/backend/domain/book/service/BookService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f309236bb2d89b728e0513f8dfa19c47f770b2a5f3888046c9e14db2735d1597
-size 1782
+oid sha256:632d86b399263b4e4e50a732c6bc4c4da52ee8c780d437aa567035bcd1aa65b1
+size 1796
