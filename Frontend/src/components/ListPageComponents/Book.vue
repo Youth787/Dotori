51bb4e55416fd12c0280f4f187661bc27ddaa74f
이하은 <hahi1108@gmail.com>
2024-02-01 20:10:13 +0900

    fix : ov-connection의 front css 반영
    Former-commit-id: ae2aab63ac27ea1179e2624cce8ded4078aa6ee6

--- Frontend/src/components/ListPageComponents/Book.vue
+++ Frontend/src/components/ListPageComponents/Book.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:147ce3e5f6e5d0c0a59b14997c6b6a5588605b3c4df90821ce812631f8c21a57
-size 2041
+oid sha256:69f80afbb9ac359a0a449e268daa63ed2871699c71fe8512ab0833af41c62893
+size 2020
--- Frontend/src/pages/ListPage/NewRoomModal.vue
+++ Frontend/src/pages/ListPage/NewRoomModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bec4e643e00320c8d89e1fbb715ac4df7f5576c4b8deec3e9f28e96432bf3eb0
-size 6202
+oid sha256:0385898764be5bd86fd03ff3788d77b9dfec5d9b0c4314f0d253caed9faf83f8
+size 5365
