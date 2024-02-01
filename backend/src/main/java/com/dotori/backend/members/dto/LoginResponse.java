b75ddddcbc7f6de6a4515b26d9f30ee4af2a0943
문준형 <moonabcd@naver.com>
2024-02-01 10:14:21 +0900

    rebase: 2
    Former-commit-id: 2fd7ca9fb0d7c52c2096b783efcb540dafaaddb1

--- backend/src/main/java/com/dotori/backend/members/dto/LoginResponse.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:046760583a8ab5dadd30275fe1c8ca35ab3fe0c9348b5a55fa3c5bab604d317e
-size 263
---
+++ backend/src/main/java/com/dotori/backend/members/dto/NaverOAuth2UserInfo.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b2fafecdb353191e123a7c7432e8e8eea4bb0035945e77f94e12487b1de9ec5b
+size 849
