d4bb6ed85492468de171c7dbb958cc2282398132
jinooyang <jinooyang@gmail.com>
2024-02-02 14:38:28 +0900

    feat : recording page added video(untested, undesigned)
    Former-commit-id: 4d51e5b569944ddc129930970cde024b4e997ded

---
+++ Frontend/src/components/RecordingPageComponents/PublishMyVideo.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a6105284200e1c4159c52f89cdf6e8d5ef82c9d450640caf39990f7e5a402bfc
+size 14109
--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fee6d1876c5bf6d5601ff5b453b68968746464640fa4b2e0d092892f70b3ed1d
-size 5219
+oid sha256:1e9c47722d344189963565c3fe785cb46f7a01cf49616f6fd64fb869f7fe5f07
+size 6037
