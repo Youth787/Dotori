0d13ddb96e021a25de31083bac32fdae4b1b90ec
jinooyang <jinooyang@gmail.com>
2024-02-02 16:01:44 +0900

    feat : 연극 페이지 화상 연결 성공 + 탈 입히기 성공... CSS필요
    Former-commit-id: 3e83740bc2f7663e1eba7d5cb2341c7ae209e5ac

--- Frontend/src/components/RecordingPageComponents/PublishMyVideo.vue
+++ Frontend/src/components/RecordingPageComponents/PublishMyVideo.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6105284200e1c4159c52f89cdf6e8d5ef82c9d450640caf39990f7e5a402bfc
-size 14109
+oid sha256:bd91a4ce369f9cf22e44c472c195a2de336769d408b56310d44a771254887d32
+size 14207
