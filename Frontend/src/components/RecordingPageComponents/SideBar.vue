a740e82e8f21e37eea8fe6250014ae5cf3c7e8af
jinooyang <jinooyang@gmail.com>
2024-02-02 11:09:48 +0900

    design : recording room 디자인 변경
    Former-commit-id: a95045de6899ae469d7f64386f1b4195f3d65570

--- Frontend/src/components/RecordingPageComponents/SideBar.vue
+++ Frontend/src/components/RecordingPageComponents/SideBar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a916aac9e4a8358e3694b42cb25f4f6580e33df3bdfad9f9a8c31194ae5beadf
-size 3751
+oid sha256:c45635d49cb11d0701e5a0f90bb41ef01fbb14aa43ba0e42b7626a0d91742e83
+size 2358
