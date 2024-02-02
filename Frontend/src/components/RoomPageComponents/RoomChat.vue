cda89802a4582ed9b4eddd2d7a6de0c0661d3dea
yeonmi <ar77gt@daum.net>
2024-02-02 13:59:48 +0900

    feat : transfer info
    Former-commit-id: 27cc0bd130a3f6411e013c2dde608068294b537f

--- Frontend/src/components/RoomPageComponents/RoomChat.vue
+++ Frontend/src/components/RoomPageComponents/RoomChat.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aab9fdc8580d610d5174d4abd0a9a0ec6008a10900d0ffb64e3b118a9375239f
-size 1801
+oid sha256:e67ab99d15a13f2fa64bc87bd1680607a0c4e173bb32f14946ba30193da10cb1
+size 2796
--- Frontend/src/components/RoomPageComponentsTemp/RoomTitle.vue
+++ Frontend/src/components/RoomPageComponentsTemp/RoomTitle.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:478a92ad55f121f5a630829e57640ea79562d5a9b997178df1c8d5a528411759
-size 749
+oid sha256:51b75faf0d7752fdb32afd07b501df2501851e7c44c724da68a4d2f8180e5e30
+size 740
