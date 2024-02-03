3d2b700adab9b93f09c0c304a5d48c027df32d77
이하은 <hahi1108@gmail.com>
2024-02-03 16:48:51 +0900

    fix : room entity getter 적용 안되는 버그 해결(boolean -> Boolean 타입으로 변경)
    Former-commit-id: d5032a843e3d7f9a866e876d98c730522132bee6

--- backend/src/main/java/com/dotori/backend/domain/room/model/entity/Room.java
+++ backend/src/main/java/com/dotori/backend/domain/room/model/entity/Room.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb5ef595dd15258b2eb5bd3201421e11d878a4fa4b18b100d90bc8ce7cf4dfc2
+oid sha256:8c86eae0d3d59955a1dc676bb8b13ec074e00a2cf416ee52901e9a898150bf9b
 size 2179
