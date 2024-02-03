be5af9548bce88dc9c8566e693e1a50bfbdb1c25
이하은 <hahi1108@gmail.com>
2024-02-03 19:11:22 +0900

    feat : 역할 선택 로직, 역할 중복 선택 시 처리
    Former-commit-id: 694732fe9efe9ebf41fa747a29c6aa37d30fa379

--- Frontend/src/pages/RoomPage/WaitingRoomPage.vue
+++ Frontend/src/pages/RoomPage/WaitingRoomPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c791e1bdf4b3b4713536f7cf024b680d9d7830a4271d792b4097da6eb5caee04
-size 2615
+oid sha256:b89ea6eeee5f823b26eb38e6b3cb31a19f8c90618972349cdd271c40f533ddcd
+size 2948
