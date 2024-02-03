c8f6eac2b97c7522256123f53a5060524117bf6a
이하은 <hahi1108@gmail.com>
2024-02-03 19:05:42 +0900

    refactor : 불필요한 주석과 변수 선언 삭제
    Former-commit-id: ac32d238df8dc8bb7ddab9f400c78597d17e9b7d

--- Frontend/src/pages/RoomPage/WaitingRoomPage.vue
+++ Frontend/src/pages/RoomPage/WaitingRoomPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7575edc95f3f6f30cb926be83c0df565fd0322a62c2bf259001c33f60807fc1
-size 2643
+oid sha256:c791e1bdf4b3b4713536f7cf024b680d9d7830a4271d792b4097da6eb5caee04
+size 2615
