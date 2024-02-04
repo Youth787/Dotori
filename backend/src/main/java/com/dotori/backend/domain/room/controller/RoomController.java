81563c942c3cfdf4543f198668232910119ac0ad
이하은 <hahi1108@gmail.com>
2024-02-04 17:34:40 +0900

    test : cors 문제로 patch -> post mapping 임시 변경
    Former-commit-id: 9f48d661d38e2e7a7eaffd8fbbee6c3d3453ad0d

--- backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ccdd4ade84a5c009b60e9c1b5f4bfd5d982d3ca1e0b86fa03c396b6371e6b36
-size 5960
+oid sha256:417760e8be18605889af1e121fcbc0ffdd2f800e5023facaa043b4d20646932a
+size 5997
