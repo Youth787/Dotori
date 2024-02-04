cc3a70fb04ce531b9b931d07074426621e1d39bd
정연미 <wkdus885@naver.com>
2024-02-05 02:04:22 +0900

    feat : add playerlist
    Former-commit-id: 3047b555fda5712dcfcce0d83d874bfa8fe4a358

--- Frontend/src/pages/RoomPage/WaitingRoomPage.vue
+++ Frontend/src/pages/RoomPage/WaitingRoomPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c6dce5cb9e19c53e06003d6fb662bbf9995c8b1fe4f029eac58c6e1579e19bb
-size 2363
+oid sha256:cfe8ee170920ba3fa72107d6cfe5ac72cf33fb99720164f354a1bfe9527d8f4a
+size 1977
