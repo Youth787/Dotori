c3d8fceb287637793f222c59ae9bd9174e0d4c69
이하은 <hahi1108@gmail.com>
2024-02-04 20:23:09 +0900

    feat : openvidu 서버로부터 역할 정보 업데이트 이벤트를 받으면 유저의 역할 선택 리스트에 반영
    Former-commit-id: a6637ede04c508de2616090f8cfb9d18666ce2dd

--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b84e3dbd0ab07ecbe6007e8624b3bc9bc416fd84f4884865388bd0ff1c97d5f
-size 11562
+oid sha256:7c11c1f02c6b4998938f91d088d02fad03bf2b97be601b4c37829bf834b5d0ef
+size 11803
