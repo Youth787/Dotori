457be72748baf10de74b142f5db3943622ce1909
이하은 <hahi1108@gmail.com>
2024-02-04 20:17:19 +0900

    mod : 역할 정보 속성 변경
    Former-commit-id: 9e859b91ffed671bfa589c16158a00347348c706

--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84ec453c9494faf3f4c59d73e15ba60c57f5171b6c38d6b4b330c9b31af446c5
-size 11271
+oid sha256:35cf960ff1dcb9b77863f45b6db0fba3426487a001c935b7a1c6e68dd2ac0b50
+size 11415
