1a9a1900730b8314b7461cb45ed95018834d6bc7
정연미 <ar77gt@daum.net>
2024-02-05 01:46:13 +0900

    feat : enter room
    Former-commit-id: ff78190bc948a095c7a55c8526bb27d7979f74b7

--- Frontend/src/pages/ListPage/RoomListPage.vue
+++ Frontend/src/pages/ListPage/RoomListPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:022149f5bfd9fc3e5b8baaaf8fa91dc98911214cc9b661e7735a1985a87b259c
-size 3213
+oid sha256:8c00c03495fcff68e8a5c021f821b6c2bb6cde528d57331372e8b791e548250a
+size 3201
