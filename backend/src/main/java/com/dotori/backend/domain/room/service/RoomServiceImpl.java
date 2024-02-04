5c6d29b82f545f6777fbf248190b498fdc4177fd
이하은 <hahi1108@gmail.com>
2024-02-04 17:14:26 +0900

    refactor : Optional 객체 예외 처리
    Former-commit-id: 6b73429df5e388a3bbc0832b5a0fcdaf7051a131

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02a79dc61c03a7d43ede72e0629fb1c4a368da46dde283d2c9808755a8ef38ed
-size 7558
+oid sha256:7c36213b8d249d60cffce0db5dd7e14b67b85a4350d1a6d18c9b6b8a48eeae04
+size 7367
