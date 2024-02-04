c0701e22ee995fb5bd80888f1f005ff0e3234574
이하은 <hahi1108@gmail.com>
2024-02-04 23:45:00 +0900

    rebase feature/ov and feature/ov-waiting-room
    Former-commit-id: 30fba79dba2be37254821551d225c251b1891c5e

--- Frontend/src/components/RoomPageComponents/PlayerList.vue
+++ Frontend/src/components/RoomPageComponents/PlayerList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa55bebee2dd8203b3dd398e3c2ef0c0aa7361535b61c14235e1b092693f5d86
-size 6456
+oid sha256:e056fdd221cdd95471b8a3492e1faf9cf606818700a90baa30b1223fbb93e53e
+size 6566
