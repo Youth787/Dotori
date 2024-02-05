24058b5a3f35f29336352a9f5b91b57f0b68df08
문준형 <moonabcd@naver.com>
2024-02-05 11:57:23 +0900

    fix : test터지는현상 수정(redisbean수동주입)
    Former-commit-id: d1d8a5cbad1f582b172cc4d89a6eb52d12791200

--- backend/src/test/java/com/dotori/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/dotori/backend/BackendApplicationTests.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f00cf980936de39f1f54031ea469faa910360458c1f2186dd4773157faf0d4a
-size 220
+oid sha256:d24926980315d8e643728643275e9d1f8d363b6fa276b147e7cb5f940cdfb1da
+size 425
