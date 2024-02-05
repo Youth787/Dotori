fa41b7f90df591206d36b6ff2a3091fc5ddb8663
이하은 <hahi1108@gmail.com>
2024-02-05 13:55:15 +0900

    feat : 방 정보 단건조회 api
    Former-commit-id: 97a61e7989feebc9b7fdb437866b52034883134f

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7b39d3cb2ec577252dd6e1f73ffa2ced047dc98957e0e0431505e764304e1fb
-size 7612
+oid sha256:641d634e712b5205fd5bc9bafb214cbd49955dbb1eb6e3d1c1216603536f721d
+size 7783
