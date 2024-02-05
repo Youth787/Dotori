46deb3d0c97585d53869edd17db1b08e01a49151
정연미 <ar77gt@daum.net>
2024-02-05 17:24:05 +0900

    chatting each other
    Former-commit-id: f92b14994dfdf6bb58d666e90506565bd9931b9e

--- Frontend/src/components/RoomPageComponents/RoomChat.vue
+++ Frontend/src/components/RoomPageComponents/RoomChat.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61fb15b9f8c51d5d970962ddb975dfce963be32d25cdd428ada9a33b53f852e3
-size 2462
+oid sha256:c64f7ffffce3cd79dc609bb1c896cc52690a3d0db752f6624a6d41f886ad3bec
+size 2557
