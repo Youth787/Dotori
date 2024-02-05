4c04f3deeb04e2daad12510d69b15df162515340
이하은 <hahi1108@gmail.com>
2024-02-05 15:20:27 +0900

    design : 채팅 css
    Former-commit-id: 33c5a2b27b68134187f7ce303447df2b4bbe01a7

--- Frontend/src/components/RoomPageComponents/RoomChat.vue
+++ Frontend/src/components/RoomPageComponents/RoomChat.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f064cc8acf0603f4101f1e2cb2dc83f97defed51b9504a95235cbeb7e4b08af
-size 2611
+oid sha256:972008b18a0d2ec7ad53e8f78bc6be1236d623e932cedb0777c2935def175b0e
+size 2613
