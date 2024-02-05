8b9aaa602ee543c7751a764d2c6251e63a64f32d
jh <bndgc@naver.com>
2024-02-05 14:56:37 +0900

    mod: 변수명 수정 videoManageSceneService videoManageService
    Former-commit-id: 063cad844dea15bccc6d477d98a882e3ddcd2b1c

--- backend/src/main/java/com/dotori/backend/domain/video/service/VideoService.java
+++ backend/src/main/java/com/dotori/backend/domain/video/service/VideoService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d448d629357ed0309960798374899fd8e8388bd9162fd8a70b8e9bb0bfaf298
-size 1154
+oid sha256:9172687074cf67fedd4911e459865ed7a2ffb7145fe2e2d383fc3718949aaa58
+size 1144
