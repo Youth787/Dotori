e8c0b4708bad263f412b032129b204b81d733079
이하은 <hahi1108@gmail.com>
2024-02-05 17:10:23 +0900

    fix : 방 참여자 역할 선택 화면 동기화
    Former-commit-id: a2909d0d5345dd2af88cc1cfc6de358eb26b38da

--- Frontend/src/components/RoomPageComponents/PlayerList.vue
+++ Frontend/src/components/RoomPageComponents/PlayerList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a70e797b7cb14e89a9e8a0d00f03244f5ab6764499ec43d059c13198fb9466c1
-size 6323
+oid sha256:476c6964c8a1e72de559706732631c36ed147019050288e2aba2cb54f6edfb78
+size 7271
