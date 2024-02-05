999bd8bbaf4fe87bbf54db95ed843e8ed2289772
SeokHyun Cho <doongdang@naver.com>
2024-02-05 22:26:53 +0900

    test : getBook 요청시 path variable화
    Former-commit-id: 26592d9f27b2a2899a392bb649a47b6148313b3a

--- backend/src/test/java/com/dotori/backend/domain/book/controller/BookControllerTest.java
+++ backend/src/test/java/com/dotori/backend/domain/book/controller/BookControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7e19a2d0dd5f31019c40c478e6992e75eb143c7ea7e13173cbdde9af40fac1f
-size 10917
+oid sha256:2f7c7130f03a57e13ea0fdb9931ef135c02d2bc1e0365ee3c44f4b30df4df671
+size 10943
