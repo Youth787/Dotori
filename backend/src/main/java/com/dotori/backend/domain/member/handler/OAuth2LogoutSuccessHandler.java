fb418ca40c2e01cc3f02d10d84bc4f2c46a1c14d
문준형 <moonabcd@naver.com>
2024-02-05 09:15:22 +0900

    fix : response가 빈상태로오는현상해결(filter수정,cors옵션수정,webconfig삭제)
    Former-commit-id: 7aad0f8debf924f7b8066bfa65bf1e1adfb4ff4b

--- backend/src/main/java/com/dotori/backend/domain/member/handler/OAuth2LogoutSuccessHandler.java
+++ backend/src/main/java/com/dotori/backend/domain/member/handler/OAuth2LogoutSuccessHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0a482b499ca05663d880f737a3ed12e2036585487d2bbc06a966715e53c77ce
-size 1767
+oid sha256:542b919eb6fe718530ead621ce9317f49993719f658fab1a35a6799ab10735ff
+size 1881
