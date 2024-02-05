7bfe9a830dfebfaf0f1c045b75290e4eb6dcc513
이하은 <hahi1108@gmail.com>
2024-02-05 13:04:47 +0900

    mod : template 에서 props 접근 변경
    Former-commit-id: 356f0f6c3481de2fd1b0e86777618dae9e49f184

--- Frontend/src/components/RoomPageComponents/BookInfo.vue
+++ Frontend/src/components/RoomPageComponents/BookInfo.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2ecab32785a2b8cc16d5ac29fcc8b61d8c6ff4569d06590e0af79901fe99111
-size 1208
+oid sha256:d1f920b5925b90028aec336c906e3e13930cf7e390d0993571e874b612ed869d
+size 1184
