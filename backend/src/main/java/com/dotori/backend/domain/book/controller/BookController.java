bc8acb0f91c85e0c3106a2da4fc4d03dda644d05
SeokHyun Cho <doongdang@naver.com>
2024-02-05 21:23:29 +0900

    refactor : getBooks, getBook service 계층에서 응답 dto가 아닌 entity dto 변환으로 변경처리 및 책 정보 조회시 책과 역할을 조회하는 메소드를 분리하였습니다.
    Former-commit-id: b2a6b526e34ece8446c49eda5b28236954811131

--- backend/src/main/java/com/dotori/backend/domain/book/controller/BookController.java
+++ backend/src/main/java/com/dotori/backend/domain/book/controller/BookController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6758e04866a55778a62838b9d7ad0315e6c8545b8cbeec064051d2975f32cef
-size 1782
+oid sha256:3253ae4e0348a0716fbac5c73dd46a60aaa2e7d6a3470846081479c7fe4f9c29
+size 1853
