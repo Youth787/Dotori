bcddad2f2efacae8207b1c3add70eb6003d9b3dd
이하은 <hahi1108@gmail.com>
2024-02-05 22:49:47 +0900

    test : 동기화 테스트를 위해 console log 추가
    Former-commit-id: 7ab47ccd1a6b210823ebf184e77c16ba0970a339

--- Frontend/src/pages/RoomPage/WaitingRoomPage.vue
+++ Frontend/src/pages/RoomPage/WaitingRoomPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2b0a36a548fefa99934c720ff7cbea3cb00b28571c6d019eaa0ba38f6d6f40d
-size 2472
+oid sha256:0ad20f2d215b9732ee508972231a39a16079f474e850da3f31b76163c197ee72
+size 2573
--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4957c0bc35efd15adfddfadc3eace029ae857a47022e4b64e9e53fce0e1290fc
-size 14210
+oid sha256:34cdc0168e6fe45c3a1c0df66613f465140b0b82c034f5fd61f2eb283631ad38
+size 14540
