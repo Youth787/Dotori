7f78b87e69160df792afec24a8d1d58f0ee96212
문준형 <moonabcd@naver.com>
2024-02-05 16:46:46 +0900

    chore : class명변경
    Former-commit-id: f950feb1380bdbaaa39eeb643477e0f0a39a3857

--- backend/src/main/java/com/dotori/backend/domain/member/service/CustomOAuth2UserService.java
+++ backend/src/main/java/com/dotori/backend/domain/member/service/CustomOAuth2UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1ca55e6aa8d6508f4427954be1446ac8b32b1dc437a53e2454eb4d4673f1a03
-size 4755
+oid sha256:b1c403cf4554c32403bfaa0a593f8e897754d6c75aa1d5f95348227407f6c18b
+size 4760
