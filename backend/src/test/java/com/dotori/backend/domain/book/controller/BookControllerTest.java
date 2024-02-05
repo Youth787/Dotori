0b81171eaf61f926f9916cf38920febae4708e08
SeokHyun Cho <doongdang@naver.com>
2024-02-05 21:50:38 +0900

    test : getBooks, getBook 검증
    Former-commit-id: 33dcbd73728ed053940a2839898315e2040e0e92

--- backend/src/test/java/com/dotori/backend/domain/book/controller/BookControllerTest.java
+++ backend/src/test/java/com/dotori/backend/domain/book/controller/BookControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a9751ab593138f6d7a013daacbb44b8f4bf3c1a12bb92dc7b063b0994d83734
-size 8150
+oid sha256:e4096d89b7a0c1d6cf0a08b02e6da3f726b209f676cae7b54760faeaeeb0ffe8
+size 9700
