372d6466dd61ff842302785023c8826b84de1a5f
이하은 <hahi1108@gmail.com>
2024-02-05 10:25:46 +0900

    rebase frontend and feature/ov-waiting-room
    Former-commit-id: 27fe882cb6fe5b468a66f1d399799f81b23ac93d

--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ded19523353d7c5a696872a4710fc505c304aabdd98d462a1b2faf70dbdd7232
-size 11809
+oid sha256:335f26d3c1f95aae94d962d10efe66c66a9a0d2546a0e83c710d78e369ba9c4d
+size 12394
