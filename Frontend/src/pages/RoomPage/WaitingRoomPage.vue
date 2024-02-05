16073aae948afdf70d82d8d21d1ed59bac69ed9c
이하은 <hahi1108@gmail.com>
2024-02-05 09:58:54 +0900

    rebase frontend and feature/ov-waiting-room
    Former-commit-id: 98b9469050064ec9a9c4d866a02533c6d5ed4e85

--- Frontend/src/pages/RoomPage/WaitingRoomPage.vue
+++ Frontend/src/pages/RoomPage/WaitingRoomPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71a36de17b9bf45a30c6fd1eb2f5e40483fb3ce3d48bfd1833a83335f5434d6b
-size 2915
+oid sha256:056a92e125fbc2a7494318529958956ff3640a95f4fd529a23686aeb7be0027d
+size 3019
