a07eedad2fc8bf02ed8ee40b72298c133dc77c92
이하은 <hahi1108@gmail.com>
2024-02-05 14:35:57 +0900

    merge
    Former-commit-id: 525c1420ae9499d00afcca92887bac89933624b6

--- Frontend/src/components/RoomPageComponents/RoomChat.vue
+++ Frontend/src/components/RoomPageComponents/RoomChat.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38a15031bc1aea434ab5cb75bf60f643ea5003b22ee09c1e5324324cf4cc4385
-size 2591
+oid sha256:d3bc9199caf1ee12ace7c93dd8fcb30dd60eaafa7c6b47eef315e87ee33cfa8e
+size 2628
