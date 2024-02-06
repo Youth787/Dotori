da9deb4ff37bf327840b90671a8f8bb3557a0f59
이하은 <hahi1108@gmail.com>
2024-02-06 19:04:19 +0900

    refactor : 변수명 카멜케이스
    Former-commit-id: 7235cc7d32693f0e8dce1d035ad3531f596294dc

--- Frontend/src/components/RoomPageComponents/PlayerList.vue
+++ Frontend/src/components/RoomPageComponents/PlayerList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b9931143bf7a89c45e56bef662657f19feae46362fa5649bbea74246f7ed7ff
-size 7075
+oid sha256:5f2b717a1fb1cc1324b13f04888c1273026f7760c89915dfa4bbf8a4c2aa5b7c
+size 7061
