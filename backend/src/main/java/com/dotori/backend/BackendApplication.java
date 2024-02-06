b02d8d61061abb0462afaf3d3203545347660006
문준형 <moonabcd@naver.com>
2024-02-06 12:10:51 +0900

    fix : redis에 session저장안함
    Former-commit-id: cdb04fb7ce2760e164b5e0e6d6b0d1a15faa26c5

--- backend/src/main/java/com/dotori/backend/BackendApplication.java
+++ backend/src/main/java/com/dotori/backend/BackendApplication.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47a25efbc47fe6a17bb8f6e578810906f43fc11a97badcd1f0d0571b365b6158
-size 524
+oid sha256:2c5f3bbf6e31aeef43de0ba7b4a9afd559908efb537d26c5ce9f5ef37e095c47
+size 403
