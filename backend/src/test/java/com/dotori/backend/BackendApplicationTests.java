65c6569e7b7ead56cbd28ffc2de6c0530fa7142a
문준형 <moonabcd@naver.com>
2024-02-06 15:03:59 +0900

    chore : text 레디스제거
    Former-commit-id: d7a39949fea9df48b6baf9234b5da6702514e121

--- backend/src/test/java/com/dotori/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/dotori/backend/BackendApplicationTests.java
@@ -1,16 +1,3 @@
-package com.dotori.backend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-@SpringBootTest
-@EnableRedisHttpSession
-	// @Import(TestRedisConfig.class) // TestRedisConfig를 로드하기 위한 어노테이션
-class BackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:7fdfb46c8829ceb303e2b3d3331bd0fb69ec1a364b7735ad31e46ac80dc5c30f
+size 304
