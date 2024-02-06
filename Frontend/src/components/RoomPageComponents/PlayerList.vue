b477444817fa9bdc85dc3162c17dabe2cdd8278f
이하은 <hahi1108@gmail.com>
2024-02-06 11:57:21 +0900

    comment : 자신의 프로필에 대해서만 역할 선택 가능하도록 하게 하는 코드 주석 해제
    Former-commit-id: 7a22cd6e588eb588319820e40e4ae7799a04494b

--- Frontend/src/components/RoomPageComponents/PlayerList.vue
+++ Frontend/src/components/RoomPageComponents/PlayerList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4260a204adbfd9016f065d87989795a0a6ac57c3f056b4481d3c3a62081f5de3
-size 7229
+oid sha256:81757030a60e0dff327c097ca6a8a030c511cd4054e7f48c60471fbd25696d0f
+size 7050
