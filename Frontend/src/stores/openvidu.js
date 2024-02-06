690db88aeda5f79808d75a244e65841bdfbd689a
정연미 <ar77gt@daum.net>
2024-02-06 10:15:04 +0900

    merge
    Former-commit-id: 52907ab42abc8851213a0a0534ad2bbda04a5ba4

--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b18442486f1746d9320573bb25d6bf9dd6dba35cdbf444224c45d5db19540b1e
-size 12639
+oid sha256:50fe574f45498cb21d13cb735675b660a7df357c3ad25a9a7829bcf557cb8b26
+size 12829
