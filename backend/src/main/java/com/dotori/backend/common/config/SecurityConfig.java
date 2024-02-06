eba2be97542ce8a9b8d581d18d2582f3d4124ab0
문준형 <moonabcd@naver.com>
2024-02-06 16:36:46 +0900

    chore : ROLE별로 페이지접근제한설정중
    Former-commit-id: 0ce8c308f7bdca60c690020c53bf6b40a0224d2c

--- backend/src/main/java/com/dotori/backend/common/config/SecurityConfig.java
+++ backend/src/main/java/com/dotori/backend/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ed12469f4ceab5b35cc992c55717e1665dd460314fd895027674563f3bc3193
-size 6168
+oid sha256:ae67c560c5086094633a570bf942bf96dd295090eac1b7bd8b2c0cadf18d2e96
+size 6450
