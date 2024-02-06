717eb94af309816e0cb112cae46ceee232de71a3
문준형 <moonabcd@naver.com>
2024-02-06 12:09:57 +0900

    fix : 중복으로 dofilter진행되는것삭제, config옵션 session stateless
    Former-commit-id: d10262485f01d24cfd2ecd30dd69c9d4d328c140

--- backend/src/main/java/com/dotori/backend/common/config/SecurityConfig.java
+++ backend/src/main/java/com/dotori/backend/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5c984e265db04f007fcf6e47f94c76b28ad108b5f86c6742bfb9a9204621d35
-size 5196
+oid sha256:82560df0d07306e289981c8e72a866d5ef8ff5f3b91f7ce79b674df1e12e1a35
+size 5193
---
+++ backend/src/main/java/com/dotori/backend/domain/member/service/LoginService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c4a1a618c9de9ca60378ffabbb7a850274066591cd328152f3dc3f89df89688f
+size 1045
