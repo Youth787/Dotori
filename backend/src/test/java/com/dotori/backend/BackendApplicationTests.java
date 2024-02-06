b2ffa08249f7c903aa29c27ec6c9740d1583529b
문준형 <moonabcd@naver.com>
2024-02-06 15:03:59 +0900

    chore : text 레디스제거
    Former-commit-id: 9e56218d494c50ad55dd98e5c9199d2a32a8a6ad

--- backend/src/test/java/com/dotori/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/dotori/backend/BackendApplicationTests.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d24926980315d8e643728643275e9d1f8d363b6fa276b147e7cb5f940cdfb1da
-size 425
+oid sha256:7fdfb46c8829ceb303e2b3d3331bd0fb69ec1a364b7735ad31e46ac80dc5c30f
+size 304
