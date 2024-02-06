60d77bd4fea1061381a34fdfb0e965eb1718a84a
이하은 <hahi1108@gmail.com>
2024-02-06 14:20:47 +0900

    mod : 중복코드제거
    Former-commit-id: 2590af508f195f63a2ea7fde7beaa262fb22055f

--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6cf80738d44892ae1f4738ceb0d21b1a641ef9c3b068a3c5f972d3dd14c13187
-size 14535
+oid sha256:7898d5043f62fc087eff55d3bc6028047a57864a6073be6ece761ab3e8c7030e
+size 14413
