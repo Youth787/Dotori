97711771c4eca9f666ef26e4a1aae1601242f2ae
이하은 <hahi1108@gmail.com>
2024-02-06 12:01:51 +0900

    feat : 선택된 역할 selectbox 비활성화
    Former-commit-id: dea65ea79e69611e38fe1e77b2abaebdeb602a0c

--- Frontend/src/components/RoomPageComponents/PlayerList.vue
+++ Frontend/src/components/RoomPageComponents/PlayerList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81757030a60e0dff327c097ca6a8a030c511cd4054e7f48c60471fbd25696d0f
-size 7050
+oid sha256:2b9931143bf7a89c45e56bef662657f19feae46362fa5649bbea74246f7ed7ff
+size 7075
