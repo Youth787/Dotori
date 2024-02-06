6f3124c7aa1385214b94a9b5e28a0417b3b0c02d
SeokHyun Cho <doongdang@naver.com>
2024-02-06 12:03:07 +0900

    fix : 방 참여시 책의 모든 정보 조회
    Former-commit-id: 984ecbf7e2006d94b5b50295e965c70479ad6614

--- backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
+++ backend/src/main/java/com/dotori/backend/domain/room/controller/RoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:417760e8be18605889af1e121fcbc0ffdd2f800e5023facaa043b4d20646932a
-size 5997
+oid sha256:4cee8719537704774030b8e9b20b52c9aa56c3de9e6095f9e5ff0aa893217f09
+size 6555
