bbd52aece871b06ad6cf2234052a36254002b76c
jh <bndgc@naver.com>
2024-02-06 09:06:07 +0900

    mod: Room 가져와서 video 저장하도록 수정
    Former-commit-id: d2ca617790f106c786dc8c28884a503a12a443fe

--- backend/src/main/java/com/dotori/backend/domain/video/service/VideoService.java
+++ backend/src/main/java/com/dotori/backend/domain/video/service/VideoService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9172687074cf67fedd4911e459865ed7a2ffb7145fe2e2d383fc3718949aaa58
-size 1144
+oid sha256:123f9de5742b53e76332a62230837b92006e6a8f3bf715be72e627e17d53dd5b
+size 1231
