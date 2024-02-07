4dfec3e39f93e8524d9a253cbc1896140b6a856d
이하은 <hahi1108@gmail.com>
2024-02-07 12:59:32 +0900

    mod : props 삭제 및 store 변수 사용
    Former-commit-id: bb754ad4b4c0b660230d13be9f84c12607ef5fca

--- Frontend/src/components/RoomPageComponents/StartReady.vue
+++ Frontend/src/components/RoomPageComponents/StartReady.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca0c4e6329754253b846c30bef2e3e73cf9cb858df0be0c74ff266648e258a80
-size 6781
+oid sha256:cc37db269cd096c7a4b610cf26c58cb6866924b068904edffed130f7c643ed5e
+size 6820
