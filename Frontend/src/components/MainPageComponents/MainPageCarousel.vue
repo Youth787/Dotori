b708546e73d7be2316b19b8b5bac8be5ee83ba61
yeonmi <wkdus885@naver.com>
2024-02-07 13:43:31 +0900

    add : mainpagevideo
    Former-commit-id: a97a12aef6f569d0ccc1ded55718b37f19776276

--- Frontend/src/components/MainPageComponents/MainPageCarousel.vue
+++ Frontend/src/components/MainPageComponents/MainPageCarousel.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e7875e0aed5b5bc26fe594cdcb0791ad58382e57273a7b13dae4351e7fb0b2e
-size 1382
+oid sha256:cb18f2758a683d9d4ab4a96c1392d8a12fcbf5258a889e565c64b24658ba45a8
+size 2039
---
+++ Frontend/src/components/MainPageComponents/MainPageVideo.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:55ad63b2994195ef8dee3700a6f12c74e67e168cc66606390edeaaa6dcbc1457
+size 736
