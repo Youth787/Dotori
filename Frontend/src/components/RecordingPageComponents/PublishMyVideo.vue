a413ccfac44b86c0d34aee3a8279fe9783cd1254
jinooyang <jinooyang@gmail.com>
2024-02-07 13:11:20 +0900

    feat : 연극방 값 초기화
    Former-commit-id: 3b7815df8ad78bf28905595325b324281a148abe

--- Frontend/src/components/RecordingPageComponents/PublishMyVideo.vue
+++ Frontend/src/components/RecordingPageComponents/PublishMyVideo.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd91a4ce369f9cf22e44c472c195a2de336769d408b56310d44a771254887d32
-size 14207
+oid sha256:eeed827d95d62c72782444c07dabdf9b8c7667c451be7bc4f5d34722049f187d
+size 14527
--- Frontend/src/pages/RecordingPage/RecordingRoom.vue
+++ Frontend/src/pages/RecordingPage/RecordingRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0c40fbd8a7dbe1b18f0c50b8e941436e85bf90b50b7659e8fd3118f8b449902
-size 2386
+oid sha256:82859f75a523dfa9cfbceb18d455ad0938abd03e3075e87b009dbe8776596d52
+size 4377
