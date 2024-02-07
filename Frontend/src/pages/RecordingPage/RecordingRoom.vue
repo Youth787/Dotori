ea342c24bd58b73f89be9762012c3856aef527dc
jinooyang <jinooyang@gmail.com>
2024-02-07 17:34:18 +0900

    fix : bug
    Former-commit-id: 7b06b5fd6e5bf00bf04410ec27069a5eb01405ed

--- Frontend/src/pages/RecordingPage/RecordingRoom.vue
+++ Frontend/src/pages/RecordingPage/RecordingRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dedf633a89e00a1d67a8a9c4373f0eda9f79321b77cd7a52fe86a8e3dacee082
-size 19748
+oid sha256:a6ab23b97644703e94720d04c6ab0289b32e596e3ff05d6122868c305e1fb1e3
+size 19751
