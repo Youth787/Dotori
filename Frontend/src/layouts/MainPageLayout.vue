80a70fd42edb510a84ae2cd52564b12ea6ceeec1
yeonmi <wkdus885@naver.com>
2024-02-07 16:53:55 +0900

    delete cal
    Former-commit-id: 8c0c9a13ada7c321e07f405b909a87a43f4d7e22

--- Frontend/src/layouts/MainPageLayout.vue
+++ Frontend/src/layouts/MainPageLayout.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a30e5d17dca2af3dd0139f2e90d69070cedf209726791a0044d5437467d93268
-size 3009
+oid sha256:68cb271e09329c8743ffdc12bf8599e1029f8c6fc285dbd2852059f3c52240de
+size 2929
