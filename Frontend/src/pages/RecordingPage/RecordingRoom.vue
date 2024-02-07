24f99b83c90ec59aecc0120095d17e4d43f79d99
jinooyang <jinooyang@gmail.com>
2024-02-07 13:30:02 +0900

    :feat : 페이지 이동 시 퍼블리시/언퍼블리시
    Former-commit-id: 6e4e7ef25303da65d10dd40f56133e49487f70e0

--- Frontend/src/pages/RecordingPage/RecordingRoom.vue
+++ Frontend/src/pages/RecordingPage/RecordingRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d1d95c8aeac2ff024ac83af86c7e8e514d06c48593b3960a1651e1c4315a44b
-size 4541
+oid sha256:a74b47fd50ae23b800bc26211e2273a340a64e552395d7bc1a9354e55c565767
+size 5002
