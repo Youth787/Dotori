a74bf767f5e3ee94c366eb2e4b97220ea0fd959b
문준형 <moonabcd@naver.com>
2024-02-07 09:50:17 +0900

    fix : refreshtoken사용로직변경
    Former-commit-id: eeb826a0b06a7e2bde81e7b19edb8370877dd711

--- backend/src/main/java/com/dotori/backend/common/filter/JwtAuthenticationProcessingFilter.java
+++ backend/src/main/java/com/dotori/backend/common/filter/JwtAuthenticationProcessingFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a54942a6be6d475f61e50cbe4331e5d31af70d03f311d74859d0acf7ced59e80
-size 8727
+oid sha256:cd4b752a187ab430b070db3191ecaae244d069bf44bc384bde0167d8404f86dc
+size 8249
--- backend/src/main/java/com/dotori/backend/domain/member/model/entity/Member.java
+++ backend/src/main/java/com/dotori/backend/domain/member/model/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07da8d860bffc9d5130932ce5fc4f6ce6ee192376e8ea7750038e4826c531da0
-size 2079
+oid sha256:95941d09360d3c4eff6ca9776be68edd61a883a56e0aa3000eb0b3d2ad49bd58
+size 2157
