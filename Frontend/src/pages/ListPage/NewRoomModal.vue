7036926ba75c71e19496251e76da5b8066eeb92b
이하은 <hahi1108@gmail.com>
2024-02-07 15:42:50 +0900

    feat : 방 참여 인원 프론트 정보에 반영
    Former-commit-id: d81a2f6d8ff92a701604197352d12172d6505b5e

--- Frontend/src/pages/ListPage/NewRoomModal.vue
+++ Frontend/src/pages/ListPage/NewRoomModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a88376b0757e66c46f31285620b4fe3738800794ff9845aee4bc3616e2f220ae
-size 6296
+oid sha256:a5362ceaf09c75c6a7775faedb30f4f56f0602e4bafb392999e9f784b3c4bed9
+size 6338
