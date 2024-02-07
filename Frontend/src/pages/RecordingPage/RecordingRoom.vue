0491eafe7240e14671826bbec3c4d53343861ed4
jinooyang <jinooyang@gmail.com>
2024-02-07 13:57:54 +0900

    update : bookInfo -> bookDetail.
    Former-commit-id: 1b6e02f9b4954f6cfe0e6ce9914a15da15e755c9

--- Frontend/src/pages/RecordingPage/RecordingRoom.vue
+++ Frontend/src/pages/RecordingPage/RecordingRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a74b47fd50ae23b800bc26211e2273a340a64e552395d7bc1a9354e55c565767
-size 5002
+oid sha256:67369a729a503e04aa847b56d0f7eb925ee51bb2aeb2a6b9ab3c2169198b906f
+size 5032
