88f42ba3671396701d0d2f51205be167d1a08a3c
문준형 <moonabcd@naver.com>
2024-02-07 14:18:32 +0900

    fix : test파일 accesstoken있는환경으로 수정
    Former-commit-id: 43d1bc4415ed203a2712561d1e1aca9b03587a25

--- backend/src/test/java/com/dotori/backend/domain/book/controller/BookControllerTest.java
+++ backend/src/test/java/com/dotori/backend/domain/book/controller/BookControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f7c7130f03a57e13ea0fdb9931ef135c02d2bc1e0365ee3c44f4b30df4df671
-size 10943
+oid sha256:46a232173e73da894666c10b03db8276cb1aec5198e0b876299e92eb37f1f998
+size 11666
