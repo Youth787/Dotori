0a5b3e02546ee6dfd33cee7e1e73ab32e9cc6cbe
이하은 <hahi1108@gmail.com>
2024-02-07 15:25:04 +0900

    feat : 책 더미데이터 삭제 후 실데이터 등록 방 생성 모달 책 역할 정보 등록, 대기방 책 역할 정보 반영
    Former-commit-id: 80317e15e72b560a57a1f096de1143fb73d4e4fa

--- Frontend/src/pages/ListPage/NewRoomModal.vue
+++ Frontend/src/pages/ListPage/NewRoomModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b85abba719ee669b1aceb4c19edbbfe5402d17bb9ad513126eefb538f78f3cc
-size 6212
+oid sha256:a88376b0757e66c46f31285620b4fe3738800794ff9845aee4bc3616e2f220ae
+size 6296
