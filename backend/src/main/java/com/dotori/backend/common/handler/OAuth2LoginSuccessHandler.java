a236cc4d1f6359865c006ad6aa7b261d59e690de
문준형 <moonabcd@naver.com>
2024-02-07 14:21:13 +0900

    fix : refreshtoken 변경 최종완료
    Former-commit-id: 01f473c482edcfa5be0748f2d4baf74c63147055

--- backend/src/main/java/com/dotori/backend/common/handler/OAuth2LoginSuccessHandler.java
+++ backend/src/main/java/com/dotori/backend/common/handler/OAuth2LoginSuccessHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f54f58615c0e6c99ad1e9656a3ebf2196929d4d1458ce1b7a805fbdbfba7849b
-size 2049
+oid sha256:5c42662831a9aeea843f82ac7ef00ac13fe70ba7ce7c51b3d1c980641251ed3e
+size 2657
