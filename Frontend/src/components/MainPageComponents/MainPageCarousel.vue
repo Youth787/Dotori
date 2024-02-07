efaea050709f88db5c234340fef93c7b134b92b1
yeonmi <ar77gt@daum.net>
2024-02-07 16:53:55 +0900

    delete cal
    Former-commit-id: 8c0c9a13ada7c321e07f405b909a87a43f4d7e22

--- Frontend/src/components/MainPageComponents/MainPageCarousel.vue
+++ Frontend/src/components/MainPageComponents/MainPageCarousel.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16f261dc2598be8557c42dd8c9db917a3229a0283cb22c4c3f23472853524be2
-size 2032
+oid sha256:e1983b0426aa30a7093c68a50f1d133ac03fed19fa4739f6b805524b6281ef43
+size 1574
