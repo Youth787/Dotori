f8fa722edf389f07340787a4c8a444c12bb0603a
yeonmi <ar77gt@daum.net>
2024-02-07 10:42:43 +0900

    add mainpage img
    Former-commit-id: 78213121a610dc257cb48d6103652acdcd9674ec

--- Frontend/src/layouts/MainPageLayout.vue
+++ Frontend/src/layouts/MainPageLayout.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14870241e03d87c0bc95216e4d176a9f8e0b75bbdeedef9f6c8420133f98777f
-size 2429
+oid sha256:a330c3c33a3d4d60fe8f1c8451d67ae9577e289629368548ee174e81b3f6b180
+size 2595
