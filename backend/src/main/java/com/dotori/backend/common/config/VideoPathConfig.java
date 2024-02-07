7204e9ea7e9fc205ec18c56a3d9bc642c59d99b9
jh <bndgc@naver.com>
2024-02-07 10:51:11 +0900

    mod: video directory 추가
    Former-commit-id: a84af62bb48c9fe11a9da6e7b4e65e5938e06dae

--- backend/src/main/java/com/dotori/backend/common/config/VideoPathConfig.java
+++ backend/src/main/java/com/dotori/backend/common/config/VideoPathConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a69fb8145c2e51c41a650f1a14360be5cbefea19473b129018d5efbce7b363a
-size 1039
+oid sha256:0613f7a5f0a883b525fe34fd1ad01a9444f7f8a535f4647e2027b182e038d498
+size 1379
