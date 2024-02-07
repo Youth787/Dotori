a75f9ea53439fd69c54cfec0646adb0fb0172d78
이하은 <hahi1108@gmail.com>
2024-02-07 15:16:51 +0900

    comment : 채팅 스크롤 마커 주석처리
    Former-commit-id: 61bf11c2f3d0064fafbb3d5a583e84ed8b6dbf72

--- Frontend/src/components/RoomPageComponents/RoomChat.vue
+++ Frontend/src/components/RoomPageComponents/RoomChat.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f2f91873d8a91fb4f364a6b5baf34652e63f7ff8c814dabaf237d0498abf3c5
-size 2933
+oid sha256:bc9bfb9fab835190d34b9f3108a0f3a648e6cc2941902f5abcdf1fb6a9e3c39a
+size 2964
