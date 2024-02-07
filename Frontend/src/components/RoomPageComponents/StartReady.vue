261bb0131872ca6e741414ae1bbfacb8c1fac22b
이하은 <hahi1108@gmail.com>
2024-02-07 13:26:05 +0900

    remove : 불필요한 메서드 삭제
    Former-commit-id: c3b64824ce387af2d016d3963fc6d57ea77e0e39

--- Frontend/src/components/RoomPageComponents/StartReady.vue
+++ Frontend/src/components/RoomPageComponents/StartReady.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc37db269cd096c7a4b610cf26c58cb6866924b068904edffed130f7c643ed5e
-size 6820
+oid sha256:37d7effc9e59b2f6fb6fd6242c40b1e301b77e0d5f72d3ae8527ddecbee1380e
+size 6849
--- Frontend/src/pages/ListPage/BookListPage.vue
+++ Frontend/src/pages/ListPage/BookListPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffbfd2e0816adfe8ff94a6baa2a3d1e3452a0a8ffc01962dce59c4dc0f50aa8e
-size 1888
+oid sha256:96145b9b37a23b5a1d15cfc4f06720419d38e00cebb7768b83a14f02bf6cf250
+size 1743
