aee5bb1035fb84535d4db23ecdc3d306330bdc01
jh <bndgc@naver.com>
2024-02-08 14:31:08 +0900

    feat: sceneVideo save
    Former-commit-id: c320c0df496ccd58b01e47cab67a8e3d7fd51f84

--- backend/src/main/java/com/dotori/backend/domain/video/controller/VideoController.java
+++ backend/src/main/java/com/dotori/backend/domain/video/controller/VideoController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bddc8924e3fa5e5eb0f9d0765a309a8a022b929538213178b2b3dbbac8a9726c
-size 2054
+oid sha256:a0db8f3874e6c02c24e816486c8987857e223952755b4f215b34a37e78bbbe58
+size 2548
