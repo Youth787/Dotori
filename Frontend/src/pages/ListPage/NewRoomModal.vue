bfc5cc3ac8b5c27b67455c0f1cef41a142dea242
이하은 <hahi1108@gmail.com>
2024-02-08 22:19:47 +0900

    fix : 대기방 공개/비공개 반대로 보여지는 bug fix
    Former-commit-id: cf918aff430e01dee252e328efe9cf2a42bc6d29

--- Frontend/src/pages/ListPage/NewRoomModal.vue
+++ Frontend/src/pages/ListPage/NewRoomModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22ca669a1473e775cc95e88e6dd5e87a1d2c840729edceafcbad26202b950745
-size 6278
+oid sha256:aa7b7a0fb3bb980560ef5a096ec0ae4387f691754192b109f55f7d8eaec85630
+size 6285
