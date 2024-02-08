86ac1aeb5eee45c16296a7cf4cd5505ec6a34509
yeonmi <wkdus885@naver.com>
2024-02-08 09:55:23 +0900

    feat : chatting scroll
    Former-commit-id: 77ef6c7c8f162bda85df10aa3240429dd3c974e7

--- Frontend/src/components/RoomPageComponents/RoomChat.vue
+++ Frontend/src/components/RoomPageComponents/RoomChat.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f2f91873d8a91fb4f364a6b5baf34652e63f7ff8c814dabaf237d0498abf3c5
-size 2933
+oid sha256:6e74397a283b072d06a5cc87654ddb3bd1d7fda38f49792fc1179ea47adcfa7a
+size 2945
