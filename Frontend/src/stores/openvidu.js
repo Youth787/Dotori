473f682e6ca386eeff9a890c4d5167f000755b78
이하은 <hahi1108@gmail.com>
2024-02-08 22:23:46 +0900

    feat : 회원 정보 (테스트용)
    Former-commit-id: e6a5d2526a2a3a17998464f8e09c16ba9e1c1459

--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:353c7a7409a88fdb33a279e7816a3cb049259f0dcf571505c4ed7759a23ba9a0
-size 7122
+oid sha256:13a01317a28fd85d4d53cf4f80b68d20d866a69cfedf7cb061ca122abf376970
+size 7154
