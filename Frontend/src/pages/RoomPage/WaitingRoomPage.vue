1a90530e6bef8424942a34ee7062e072919dfa9f
yeonmi <wkdus885@naver.com>
2024-02-08 21:35:11 +0900

    design : head css
    Former-commit-id: b5f994e023064d5d29ab266afe0446c52378d64b

--- Frontend/src/pages/RoomPage/WaitingRoomPage.vue
+++ Frontend/src/pages/RoomPage/WaitingRoomPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4226fa1cd3264b72e930dc8b073ca89ce18ef70d504873ccfc229d76c023068f
-size 4082
+oid sha256:a823b0582542f6cc7a1f91fa691529d4e5f5e40f7ed87418d6d592207df9a242
+size 4157
