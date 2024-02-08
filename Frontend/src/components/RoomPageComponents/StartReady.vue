ea1140cfe07ede6fafce617de7115a3d26e87fe9
jinooyang <jinooyang@gmail.com>
2024-02-08 13:17:46 +0900

    fix : bug (waitingroom and recordingroom)
    Former-commit-id: f9aaf854c164093e298fed5734d6b18c43f65706

--- Frontend/src/components/RoomPageComponents/StartReady.vue
+++ Frontend/src/components/RoomPageComponents/StartReady.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:152533e707588b88a20913deb646774b4022f45dc7c52c6d2c87d7f697ca1367
-size 6856
+oid sha256:581fcfbe7b189b2463bd36f6342339c7e6575e360fd71c87ed368c3fb58bb0db
+size 6927
