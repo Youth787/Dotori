838fc39ebfcf3e637dbefb59dd3f7e5dcc08a3ce
yeonmi <wkdus885@naver.com>
2024-02-08 22:43:16 +0900

    style : cursor
    Former-commit-id: 213ec266e1c816fcb8e4529228ebd15e02a8cb68

--- Frontend/src/components/CommonComponents/Header.vue
+++ Frontend/src/components/CommonComponents/Header.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b40ae34a887ced0c919108a253dfe93deac5ce8d17d97460d27c70419af360c
-size 5847
+oid sha256:c3fc03e52b4047a876f61b5e9158db9f07d6e5ef37a646376803c0990ae04b1b
+size 5870
