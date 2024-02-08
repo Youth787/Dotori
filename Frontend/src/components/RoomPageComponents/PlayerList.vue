d64bc5ef1832e1c080e38cc6c7c35b65a6771644
이하은 <hahi1108@gmail.com>
2024-02-08 21:21:26 +0900

    feat : 선택한 역할이 버튼 텍스트로 표기, 취소 버튼은 해당 유저의 것만 보이도록
    Former-commit-id: 89e6ff964303d77818e01ca521613be2229c89d0

--- Frontend/src/components/RoomPageComponents/PlayerList.vue
+++ Frontend/src/components/RoomPageComponents/PlayerList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a883112a26fa81835462f82ae6dc0a61cdeb19ef49d91cc3e2c4775e4d1cfade
-size 7516
+oid sha256:a558f49e3b7af46af4d87ebadf8c5d4334fec18d6cbb9fa541f0e52acbeaa57e
+size 7562
