2f75b29c60390ccd4556059caaf228ad543d3c95
이하은 <hahi1108@gmail.com>
2024-02-08 22:23:46 +0900

    feat : 회원 정보 (테스트용)
    Former-commit-id: e6a5d2526a2a3a17998464f8e09c16ba9e1c1459

--- Frontend/src/pages/RoomPage/WaitingRoomPage.vue
+++ Frontend/src/pages/RoomPage/WaitingRoomPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c49bb9b06db7ec1cba230b583023645b431ed6273262b6f54e2e2367b98f4ae
-size 4164
+oid sha256:6444790a15cc56224fb229d6b80bda73036d5ebcbd59f97a9d01bc30bd8f5299
+size 4192
