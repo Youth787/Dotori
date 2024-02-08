8a355274a9e95b104346d3bfea71be4e8f1e70b1
이하은 <hahi1108@gmail.com>
2024-02-08 21:23:08 +0900

    fix : memberInfo 선언 위치 수정
    Former-commit-id: 1fac1d35818649ee7418df6a352647576ffe4c77

--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a25405e6db3d66aae87e25b64dfcbbf499b1485908c6b1fa8b6f315718377b4
-size 7061
+oid sha256:353c7a7409a88fdb33a279e7816a3cb049259f0dcf571505c4ed7759a23ba9a0
+size 7122
