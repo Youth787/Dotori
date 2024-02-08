8f06752bbf34d0e3b2f8076d9220acedf67c551c
jh <bndgc@naver.com>
2024-02-08 14:41:33 +0900

    refactor: downloadVideo2
    Former-commit-id: d0972f238effbb0055d94f3d15715e93aa599512

--- backend/src/main/java/com/dotori/backend/domain/video/controller/VideoController.java
+++ backend/src/main/java/com/dotori/backend/domain/video/controller/VideoController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d00e5682f89ab425681fc6d0fe349a90b08740edc82c6b812a46cff7c5475a7
-size 2822
+oid sha256:feffb6f325bae2b9749872b6085dd52bcf9db8764dd356df121c84c84dbbbaa8
+size 2609
