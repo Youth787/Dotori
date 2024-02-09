72aa6519c71bf8a4d8fc65d63e34d05a1b1def3f
이하은 <hahi1108@gmail.com>
2024-02-09 21:44:14 +0900

    feat : 방 입장 시 방 정보 pinia에 저장
    Former-commit-id: 7a1d4b618c11f3b32efeddc0d6949c942ae1597d

--- Frontend/src/pages/ListPage/RoomListPage.vue
+++ Frontend/src/pages/ListPage/RoomListPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc0c750aecb047f8802e413d3559c0019d360fa1a2cc82202eeba31167dd234e
-size 2950
+oid sha256:b7b32dc79cecb55fccd8a17c863c7e43fe4706bfedd1df182d51cac8550e4592
+size 3042
--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91bf2dc2ec7f23d4f57b0371d3456b82688ac1315fa8e483a147050e3b3e237e
-size 8713
+oid sha256:14e3b9d76f8e8f7e1fc7ae4823d1fc9708adad9e7917d56bace00e6dd9aad1f9
+size 8709
