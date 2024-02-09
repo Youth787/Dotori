d9e67f8b4e3e5c60ea106080cd436e00f77e684f
doongdang <whtjrgus0428@gmail.com>
2024-02-09 22:09:46 +0900

    rename : 오타 교정
    Former-commit-id: cfe6e778f9763a41d87b020f52314af36e81b39e

--- backend/src/main/java/com/dotori/backend/domain/member/Repository/MemberRepository.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:60cdd07f840df1029790651f861e2571e9a0fa3b9f56f9c5a41fc0fa9b297873
-size 986
---
+++ backend/src/main/java/com/dotori/backend/domain/member/repository/MemberRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d981945c8cc5fb35e481ceb3db50e079eb6bda904cb76b06825903c1a4940047
+size 924
