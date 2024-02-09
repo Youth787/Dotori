3846a2ab072ee9a1e6e631313d034d1202e4117b
jinooyang <jinooyang@gmail.com>
2024-02-09 19:49:30 +0900

    design : image fill
    Former-commit-id: e623e7b7bc9729ebb29113217171bcdfe5e2ecad

--- Frontend/src/components/RoomPageComponents/BookInfo.vue
+++ Frontend/src/components/RoomPageComponents/BookInfo.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7362a8b1f93c3efe34b62a39a8624d2cdaac1710c1bfa8edd2c545d7432184f
-size 1281
+oid sha256:842f6b8d1348d550eaf7b7bba34eb404b35b4fff0ec4114ff847b02d5cd2c9fe
+size 1266
