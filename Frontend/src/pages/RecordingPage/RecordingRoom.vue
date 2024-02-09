92b8af3dd7b265b51e6fd5ef12d3a95de141e941
jinooyang <jinooyang@gmail.com>
2024-02-09 17:02:53 +0900

    fixed : sidebar color
    Former-commit-id: d1defcfb380108a7f1c38cb977c05f1fda26c1c8

--- Frontend/src/pages/RecordingPage/RecordingRoom.vue
+++ Frontend/src/pages/RecordingPage/RecordingRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:117601382c5580a2bd148ef791cdea1b90001216f7a16210cedf3a4718f39ab5
-size 20588
+oid sha256:8496c6d7eb0a78902dd80406a380d240bc168ce12a04bfa4d8b7b2b654e45455
+size 20596
