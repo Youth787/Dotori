610f5d7236c39ba7f1bdd1476b2dba94e39ddfda
이하은 <hahi1108@gmail.com>
2024-02-09 22:27:24 +0900

    mod : isHost 변수
    Former-commit-id: 415e5f6d707c84c52d642fe1b06ee7974547a045

--- Frontend/src/pages/ListPage/NewRoomModal.vue
+++ Frontend/src/pages/ListPage/NewRoomModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26c57c8056ff7fc2dadff433f7c0a0dea560cbfbc6f1be612a873167a5f93a9f
-size 7549
+oid sha256:82a6fd86ba7cfab366f111f032983a33db4d76de294e076f21af99d81b6251db
+size 7566
