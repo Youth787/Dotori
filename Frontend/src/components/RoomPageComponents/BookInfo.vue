9c318a937be45cbb548256a7a6ae77cbdf09019e
jinooyang <jinooyang@gmail.com>
2024-02-09 19:35:14 +0900

    fix : roomPage image css
    Former-commit-id: 7bd16ecc8ff3fae4a6c40b57e92a515776c91d1d

--- Frontend/src/components/RoomPageComponents/BookInfo.vue
+++ Frontend/src/components/RoomPageComponents/BookInfo.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e846793754f9e0f9439a7b63403d108dfa84403d363cba7402a1e38a7ab8f255
-size 1187
+oid sha256:d7362a8b1f93c3efe34b62a39a8624d2cdaac1710c1bfa8edd2c545d7432184f
+size 1281
