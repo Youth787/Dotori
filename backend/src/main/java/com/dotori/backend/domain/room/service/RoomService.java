5ffb6a24047f3254f383485fb702d233556ebb61
이하은 <hahi1108@gmail.com>
2024-02-09 21:30:53 +0900

    refactor : 예외 처리
    Former-commit-id: 67cc6ba8be7dc7ce03fa9e35005a2843174ba5e4

--- backend/src/main/java/com/dotori/backend/domain/room/service/RoomService.java
+++ backend/src/main/java/com/dotori/backend/domain/room/service/RoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76331cdf6296bf0550a74059a20d72d7cef974345fe45115440f9c1d79954641
-size 1229
+oid sha256:4cf6d22cfca1e175fdc2aa236886fbdef22af6d2ac8bea9b2d23cb74c3f2f127
+size 1226
