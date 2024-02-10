b3f8ff059f0a54d1a1c6a4078312d814c2d373ed
doongdang <whtjrgus0428@gmail.com>
2024-02-10 23:59:17 +0900

    move : dto/auth로 이동
    Former-commit-id: 13d71528459eb26572d6b8d69ba462c7d04bb3f0

--- backend/src/main/java/com/dotori/backend/domain/member/model/dto/CustomOAuth2User.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4bdb502eb97a61068a1316de15ca01e8137dfe9de53ffba2f6f194e72f735953
-size 961
---
+++ backend/src/main/java/com/dotori/backend/domain/member/model/dto/auth/OAuth2UserInfo.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8f8d8fcef76012c60a020e21666b6b9a5f991e4271f7ec3e948d6791f8ab81dc
+size 369
