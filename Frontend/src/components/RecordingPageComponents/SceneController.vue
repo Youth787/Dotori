71c4bf57d40a4357b1d8eb202ceba10cf5467c04
jinooyang <jinooyang@gmail.com>
2024-02-10 23:41:24 +0900

    feat : mergeVideo, 녹화된페이지확인
    Former-commit-id: 0f351ee91d2ab382c33c004cbe9bff1dbb1270b3

--- Frontend/src/components/RecordingPageComponents/SceneController.vue
+++ Frontend/src/components/RecordingPageComponents/SceneController.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c664f8a12a9e2f7d26549f49c82367495eef4080ee26d75baddee443eee61d7c
-size 9160
+oid sha256:a5d89ec7ce1e519ec26e3a9852239370b664e0f497c8295cf96983d78276dcb0
+size 9665
