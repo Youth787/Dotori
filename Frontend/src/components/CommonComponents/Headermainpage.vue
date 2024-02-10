bffe71dd6d6ebcd02c36e44ef765b130ba1af1f3
jinooyang <jinooyang@gmail.com>
2024-02-10 16:23:22 +0900

    design : header 스크롤 내렸을때 배경 생김(메인페이지)
    Former-commit-id: 130a8a31229114cb336c807de0a019b8fdb880eb

--- Frontend/src/components/CommonComponents/Headermainpage.vue
+++ Frontend/src/components/CommonComponents/Headermainpage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98a1cf2c0cf3eecc80c6ba149caa99f90b42f52353eb76904e5c30df668d8c33
-size 5311
+oid sha256:4641c4203268317dd5d687a42939b8d68d091504797dbf2f6919a1a79423a85c
+size 5940
