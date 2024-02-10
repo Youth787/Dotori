9e7b349eb827168ee983114fb5185b30ae4347d4
bufflect <bndgc@naver.com>
2024-02-10 14:30:04 +0900

    fix: memberController  - cors 충돌 해결
    Former-commit-id: 2a8b363b26975d4175af969e59521d81d25dd576

--- backend/src/main/java/com/dotori/backend/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/dotori/backend/domain/member/controller/MemberController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4eac8a608d590dd5e49ccecff4cd3d7629fc8739f0fceb2e2804f66f32df7baf
-size 5981
+oid sha256:00b988c062cd24311f91282bf9292385a0af3dc8ebac01f416102b2b1794cfef
+size 5931
