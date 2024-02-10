3d9701c93c18860855eef26fe30e3be8e53393d0
jinooyang <jinooyang@gmail.com>
2024-02-10 14:46:21 +0900

    design : my-green to my-brown
    Former-commit-id: 7bef2d15cabc7918733925fb0558fe38c136b422

--- Frontend/src/components/RoomPageComponents/PlayerList.vue
+++ Frontend/src/components/RoomPageComponents/PlayerList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c701795ee678d6d5eefaaa3ff931933924a9d19251babd45b7a037adedea3e1
+oid sha256:544a33925ee59ecf510f0f90fc1786030cba02c4fdf06f2dbff1e1e9e34c418b
 size 8350
