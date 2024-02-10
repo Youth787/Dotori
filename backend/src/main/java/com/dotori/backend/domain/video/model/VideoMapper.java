42f20ad36e5544b147030d96c2c91bf693250a79
bufflect <bndgc@naver.com>
2024-02-11 01:21:57 +0900

    mod: video videoId 있는 생성자
    Former-commit-id: c9073b3c7cd9c5cc6d7e81fb4a70e07a1d0d2ad2

--- backend/src/main/java/com/dotori/backend/domain/video/model/VideoMapper.java
+++ backend/src/main/java/com/dotori/backend/domain/video/model/VideoMapper.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f962690786c588361c550673161c5b2be24cc70e41594778dac478913cbccdf9
-size 430
+oid sha256:e1ca49248cc3fc0841355c33746a7b651edc4cd908f14f8d0c9e0f446e13b114
+size 465
