a744598aacfeb801137c9c3cb8503ec08269b424
bufflect <bndgc@naver.com>
2024-02-10 14:29:17 +0900

    fix: JwtAuthenticationProcessingFilter
    - 로그인시 access_token만 식별해서 SecurityContextHolder에 Authentication 객체 주입하는 것으로 변경
    
    
    Former-commit-id: 6bf8dbb24ccdf7f3a9d713cc18e0bc532f5ace8a

--- backend/src/main/java/com/dotori/backend/common/filter/JwtAuthenticationProcessingFilter.java
+++ backend/src/main/java/com/dotori/backend/common/filter/JwtAuthenticationProcessingFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da481ca7c9e285c6a3265dec94cb916ff7f13c46d650472c2781c05e17a77633
-size 8213
+oid sha256:5c369bcfac79663d063426d508f49f4f3cfe85fc3c22ac9ad4a0047a449fab5a
+size 6382
