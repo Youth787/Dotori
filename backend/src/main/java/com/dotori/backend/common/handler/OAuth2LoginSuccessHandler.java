10fc679b9cda64063486d6cf2cae3d61577919da
doongdang <whtjrgus0428@gmail.com>
2024-02-10 23:59:17 +0900

    move : dto/auth로 이동
    Former-commit-id: 13d71528459eb26572d6b8d69ba462c7d04bb3f0

--- backend/src/main/java/com/dotori/backend/common/handler/OAuth2LoginSuccessHandler.java
+++ backend/src/main/java/com/dotori/backend/common/handler/OAuth2LoginSuccessHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea9e3f9275297b917c32aa38215ecac575bc1ca02ee052798c5c9f2e4958c38b
-size 2683
+oid sha256:b6dd9b7b0e87d57bc0327d07f0534aeb25fb8ee67f47f7d94700db9edaef5fba
+size 2688
--- backend/src/main/java/com/dotori/backend/domain/member/model/dto/NaverOAuth2UserInfo.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:eeb90aaf396940319127d12820cf63d4805a75fc7aac8ef8f6369caaef0d8317
-size 861
