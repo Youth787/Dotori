9e83d231db39f950381df5e211d1a65bb772cb91
jinooyang <jinooyang@gmail.com>
2024-02-10 15:29:48 +0900

    fixed : modal css
    Former-commit-id: 411b512a063cd2805420c73025467f96002a1409

--- Frontend/src/components/ListPageComponents/Book.vue
+++ Frontend/src/components/ListPageComponents/Book.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a7b67f717c12f1db5fd20a9593ddd3acb520a45d63fefd90a93fddb5c78e2d0
-size 1976
+oid sha256:a20fa5faa2f9eed7196a5aaebec4f724b71b20c508c0c76f01d983be2c031841
+size 1811
