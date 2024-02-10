029c3e4151f5547fddb1a80f71ccacef39d0956a
이하은 <hahi1108@gmail.com>
2024-02-10 20:03:35 +0900

    fix : api 호출 메서드 변경
    Former-commit-id: 5cf7fdf5719656fac3d9e93e9b76892e4e2ccecd

--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f783b7dc492c5a6e0317d6a010cca656173ea4db22b76b0e4ed3c21a343ad173
-size 8740
+oid sha256:64e9b03650f7507cc59248834672b3eb0c3ba676d8cffb713be481775c84d415
+size 8741
