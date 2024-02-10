27c4f3c9a23af3acb432182d17767e2261642461
doongdang <whtjrgus0428@gmail.com>
2024-02-10 22:20:52 +0900

    mod : axios 통일
    Former-commit-id: 1dcc8be42c1f565e11463513ad8c2614ed35cf4c

--- Frontend/src/pages/ListPage/NewRoomModal.vue
+++ Frontend/src/pages/ListPage/NewRoomModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:752a50274cc3ae062f8af6c11256cee7e0e40a2dbaf06b77ac150ad9aaba5bad
-size 7438
+oid sha256:58b4e9d81b4e72fc820093236af2cba09f474f2686f91f480a2dc951a948c95f
+size 7471
--- Frontend/src/pages/MyPage/MyInfoPage.vue
+++ Frontend/src/pages/MyPage/MyInfoPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01cdd99ac82f7315a89e1639f05595c004d9dc7119db23bd701af6aaac7495ee
-size 5678
+oid sha256:5d3833e2680b814d30f0d7645597e3bb32da21e15d1b68a48677217bb104d6c5
+size 5595
