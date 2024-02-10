30c044b24663245d079e8fe16fd3f238100653fa
jinooyang <jinooyang@gmail.com>
2024-02-10 16:04:11 +0900

    add : added logout to header
    Former-commit-id: 7f6d7860b028765e376ccd506ce083a0e5aeeb43

--- Frontend/src/components/CommonComponents/Header.vue
+++ Frontend/src/components/CommonComponents/Header.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3fc03e52b4047a876f61b5e9158db9f07d6e5ef37a646376803c0990ae04b1b
-size 5870
+oid sha256:047e4fc852abc932fa0908ad77e8350b88cb172d2f6c76a038f39de5d73b9788
+size 5854
