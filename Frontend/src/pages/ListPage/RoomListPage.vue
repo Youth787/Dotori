7277a4a4d685efb5d33109cc087fa6f4e803aa8f
이하은 <hahi1108@gmail.com>
2024-02-11 16:25:36 +0900

    rebase
    Former-commit-id: 4a23e21f6d2e3dab2dced5121237bdd3d93c7aa2

--- Frontend/src/pages/ListPage/RoomListPage.vue
+++ Frontend/src/pages/ListPage/RoomListPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbd653c7ae74e7e0ee1b1106644ecdf9a6b92d7706027741b74cb6ef00f2b9f3
-size 3026
+oid sha256:3d08e77f0c909ea2c5db4d4a8d32ec1849a1a270ff99aa74b7560158aef8b707
+size 3056
