74ba8cb06ff761f78e0812296d10e3bab5ee610f
jinooyang <jinooyang@gmail.com>
2024-02-11 20:51:05 +0900

    feat : view recorder video
    Former-commit-id: 640957ca6d5a4a87dcb2894aa7153f078703f6cb

--- Frontend/src/pages/RecordingPage/RecordingRoom.vue
+++ Frontend/src/pages/RecordingPage/RecordingRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e06c9ba46974ed50aebb968dbce9888da6aec87dd9962f3c1bbee5967d7105a8
-size 22852
+oid sha256:710bdb584fcf5b8707718f9e84e1506e234c7b5c31e3105b256e32337ee35fdb
+size 22961
