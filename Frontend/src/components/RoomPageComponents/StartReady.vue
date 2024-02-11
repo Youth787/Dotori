9e15a0508b5e6e030ff0d1a5c5e6a448eddbea4f
이하은 <hahi1108@gmail.com>
2024-02-11 20:42:17 +0900

    comment : 테스트 위해 코드 주석
    Former-commit-id: a332352d676a7c15f419bb28044f849e2a07c767

--- Frontend/src/components/RoomPageComponents/StartReady.vue
+++ Frontend/src/components/RoomPageComponents/StartReady.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05b64b2e798a0aed60492cb3414df76e2ba228ff33f5a8fddc809549ffd89d1a
-size 8364
+oid sha256:4097ce1e2c49104ffc90d5d12ac7fe6cbffd32572c994b3498a95783e2f7b7ad
+size 8447
