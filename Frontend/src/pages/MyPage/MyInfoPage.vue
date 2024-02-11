2915424c710247df5ed53c3f74751e4dd47471bb
doongdang <whtjrgus0428@gmail.com>
2024-02-11 20:50:55 +0900

    mod : 이미지 수정 API
    Former-commit-id: e7c4e18a3857a16cd0d4378bb6546befff6f55af

--- Frontend/src/pages/MyPage/MyInfoPage.vue
+++ Frontend/src/pages/MyPage/MyInfoPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55bb5ef24c12abb8f9fb3485ce1762e832a079270700ef88a8ebb76ac6ab8cb4
-size 4839
+oid sha256:fac558bd0a4f6a85cd2e83ad2665ac12852f5df2433069313d7f01ea70f7d40c
+size 4838
