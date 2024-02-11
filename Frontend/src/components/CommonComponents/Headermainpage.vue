be482bf1b30db169210821093c6e53b018207c4c
jinooyang <jinooyang@gmail.com>
2024-02-11 14:46:55 +0900

    design : mainPage
    Former-commit-id: 785cf1048810b1b0a996c172284af572bcff1037

--- Frontend/src/components/CommonComponents/Headermainpage.vue
+++ Frontend/src/components/CommonComponents/Headermainpage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb6bcc6cf6c2552dedee0f6e7b587f897a72b4ff698f61dad9c4422525c2bdac
-size 5919
+oid sha256:d8063f71cd497b2997eef4f20802d039ba1ad575662df66c1d15aa78227237d9
+size 5917
