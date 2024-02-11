b3c83425c2ecc3a873982daa8e68cf7d8f5c4035
doongdang <whtjrgus0428@gmail.com>
2024-02-11 20:33:23 +0900

    add : 프로필 이미지 실 저장 경로, db용 경로
    Former-commit-id: 23933f07304a04e5d50bc9ae7c027a8ab131813e

---
+++ backend/src/main/java/com/dotori/backend/common/config/ImagePathConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:08ba190de30d47952de7d2009811ca3d6ab140e25818b4750dcd254c5a888466
+size 748
--- backend/src/main/java/com/dotori/backend/common/config/PathProperty.java
+++ backend/src/main/java/com/dotori/backend/common/config/PathProperty.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6916fbf2bdd81988b037be6a47e70c74cf0c5b62651d41a6eab069634584d309
-size 276
+oid sha256:a1590ea23833aa12689d974447cfecbda69404c5a74538221128bb28e2898e99
+size 353
