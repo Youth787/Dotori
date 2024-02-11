ca85fd0a5d057fced39e40b8d4d427d2ccf4b213
이하은 <hahi1108@gmail.com>
2024-02-11 17:35:29 +0900

    feat : 리스트페이지 본인 프로필 반영
    Former-commit-id: e74fd0685e0851873c0b4c67d126a56a597c37bb

--- Frontend/src/layouts/ListPageLayout.vue
+++ Frontend/src/layouts/ListPageLayout.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cfbe3253d38542ac1b25e780b53a285440393dd06afd7fbcb4f28919289bbe70
-size 3072
+oid sha256:f7df18f2199fbe5ca987d60c8277097f187c744ca575c575dd76ec35e073dea5
+size 3141
