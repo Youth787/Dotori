9b0e6f020b926c2acd7872ae21be45772bfdf888
jinooyang <jinooyang@gmail.com>
2024-02-11 16:56:05 +0900

    fix : recording sync (loading)
    Former-commit-id: 2686b6f77f0ca5d6a4bc2b12d49619e7112c2210

--- Frontend/src/pages/RecordingPage/RecordingRoom.vue
+++ Frontend/src/pages/RecordingPage/RecordingRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ab348ac02d363e62ba7a6b09bd48a000e0ff84708afb6433c4ca5e81e8917ae
-size 21365
+oid sha256:e06c9ba46974ed50aebb968dbce9888da6aec87dd9962f3c1bbee5967d7105a8
+size 22852
