3ac24eb6455ace7b1439b5fa625abdef81e3d9ba
bufflect <bndgc@naver.com>
2024-02-11 17:36:29 +0900

    mod: dotori_domain 외부화
    Former-commit-id: c61a822a69d2316a92b0e2798906d9389b378226

---
+++ backend/src/main/java/com/dotori/backend/common/config/PathProperty.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6916fbf2bdd81988b037be6a47e70c74cf0c5b62651d41a6eab069634584d309
+size 276
--- backend/src/main/java/com/dotori/backend/common/handler/OAuth2LoginSuccessHandler.java
+++ backend/src/main/java/com/dotori/backend/common/handler/OAuth2LoginSuccessHandler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6dd9b7b0e87d57bc0327d07f0534aeb25fb8ee67f47f7d94700db9edaef5fba
-size 2688
+oid sha256:f66f0af85ba1e5dcfe2f947f5433d1e7d18c492f0ec049c246faf5fbee82edc0
+size 2783
