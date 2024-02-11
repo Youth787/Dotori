bc54e1961cd2ed8d04b16a66789ccd945eea37f2
이하은 <hahi1108@gmail.com>
2024-02-11 20:37:00 +0900

    design : 모달 아이콘 꺠지는 현상 수정
    Former-commit-id: 87779867b567feb8ee51215cc7b884e18ba04e66

--- Frontend/src/components/RoomPageComponents/StartReady.vue
+++ Frontend/src/components/RoomPageComponents/StartReady.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:734e0c026c41a0059a4f3772b6a3a650f8f1fcbcc30ba77f3855d877d06cc518
+oid sha256:05b64b2e798a0aed60492cb3414df76e2ba228ff33f5a8fddc809549ffd89d1a
 size 8364
