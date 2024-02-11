da9e3eb28e016e299c279ef8115a6dd6731d2c48
bufflect <bndgc@naver.com>
2024-02-11 15:50:58 +0900

    mod: video output.mp4가 아닌 sessionId.mp4가 되도록 수정
    Former-commit-id: 22f8c2d37d090352b813a13c0739b7cbb21602c7

--- backend/src/main/java/com/dotori/backend/domain/video/service/VideoMergeService.java
+++ backend/src/main/java/com/dotori/backend/domain/video/service/VideoMergeService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d91918d1e35b3f477809554056f621f2cb1451a67ce95d9018e035213c3daff2
-size 2515
+oid sha256:f54159cdbf73bd7679d13aa000ce1ae4401f365a5f624029876e6e1d49137df5
+size 2541
