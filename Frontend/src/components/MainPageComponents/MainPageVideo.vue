c213a28835275ff419816bc060ceeaf294f7ccbf
jinooyang <jinooyang@gmail.com>
2024-02-11 14:46:55 +0900

    design : mainPage
    Former-commit-id: 785cf1048810b1b0a996c172284af572bcff1037

--- Frontend/src/components/MainPageComponents/MainPageVideo.vue
+++ Frontend/src/components/MainPageComponents/MainPageVideo.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e74f06e2c06f6ee835159f9b36387894949ec2235b4a0c1339a9ae25a931254c
-size 1695
+oid sha256:96e82a7d829a77a22e95e78d200bb21b538d8aa3a8caecb2017d90e2291cb37d
+size 1743
