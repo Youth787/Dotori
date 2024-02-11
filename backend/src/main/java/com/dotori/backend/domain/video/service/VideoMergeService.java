b92926b4b97c86be79f69101d10cdf505feb2859
bufflect <bndgc@naver.com>
2024-02-11 15:50:21 +0900

    mod: merge된 SceneVideo 삭제 처리
    Former-commit-id: eea72df508f4011b4299d40a1423a89a8ad71ddf

--- backend/src/main/java/com/dotori/backend/domain/video/service/VideoMergeService.java
+++ backend/src/main/java/com/dotori/backend/domain/video/service/VideoMergeService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bd9adeb04f6173d72bd243bed0915a964b43c0b375d25a3acfcdd134c5653ef
-size 2219
+oid sha256:d91918d1e35b3f477809554056f621f2cb1451a67ce95d9018e035213c3daff2
+size 2515
