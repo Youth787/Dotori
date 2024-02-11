5bd9a23c26f2e7f14f759a200166e4a6fcb7a0aa
yeonmi <wkdus885@naver.com>
2024-02-11 17:18:07 +0900

    feat : alert data
    Former-commit-id: c634e726491b208421f16b7b3a8fbda0fb84e4e5

--- Frontend/src/components/RoomPageComponents/RoomChat.vue
+++ Frontend/src/components/RoomPageComponents/RoomChat.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbdb2a8c0edc6c1002bce6f96f231a15d680acede4fa5f1157b38b1f5e92fe15
-size 3337
+oid sha256:e27231b0cceca54d3ad8b31a2fdc3f79aef37efffffcad79b161c4ca8b584d7e
+size 3318
