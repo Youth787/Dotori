221a14657819fc810b9b74b8edb90aba305e023c
yeonmi <wkdus885@naver.com>
2024-02-11 17:20:14 +0900

    feat : enter chat test
    Former-commit-id: 8b7f88b2b86ac7ac4be80485cb9cd50e8e194f1f

--- Frontend/src/components/RoomPageComponents/RoomChat.vue
+++ Frontend/src/components/RoomPageComponents/RoomChat.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e27231b0cceca54d3ad8b31a2fdc3f79aef37efffffcad79b161c4ca8b584d7e
-size 3318
+oid sha256:2426732488f99456bbaba0dd060053a6956a8739f565364c4b2bf28d7c2b7fbd
+size 3391
