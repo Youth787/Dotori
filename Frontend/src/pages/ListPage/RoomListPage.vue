6fa732f4f4529c6b0fcd8f5c842acb8006e73a77
yeonmi <wkdus885@naver.com>
2024-02-11 17:54:52 +0900

    feat : passwordtest
    Former-commit-id: bf5f130e05e9d3b0e77c56284f27fc87b851b5fd

--- Frontend/src/pages/ListPage/RoomListPage.vue
+++ Frontend/src/pages/ListPage/RoomListPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbd653c7ae74e7e0ee1b1106644ecdf9a6b92d7706027741b74cb6ef00f2b9f3
-size 3026
+oid sha256:77cdb00b12e12d6df2696ecfcaa531f10ee1f05178cd4244f26680df1d04fff4
+size 4412
