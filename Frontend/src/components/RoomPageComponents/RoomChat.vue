740cb28fa37c18285e7ef777c6029cfac75856ed
yeonmi <ar77gt@daum.net>
2024-02-11 17:12:25 +0900

    chat test
    Former-commit-id: fcc6eb6faaa0bfa39f2f0062bceb6b66c7715723

--- Frontend/src/components/RoomPageComponents/RoomChat.vue
+++ Frontend/src/components/RoomPageComponents/RoomChat.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e059972fa57da9add456e912139969cf5b926ca49ac646506f155daf79594b43
-size 3153
+oid sha256:bbdb2a8c0edc6c1002bce6f96f231a15d680acede4fa5f1157b38b1f5e92fe15
+size 3337
