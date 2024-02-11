867d06e708adec6e25c87a1a5f0eeb531d6dd500
이하은 <hahi1108@gmail.com>
2024-02-11 20:37:00 +0900

    design : 모달 아이콘 꺠지는 현상 수정
    Former-commit-id: 87779867b567feb8ee51215cc7b884e18ba04e66

--- Frontend/src/pages/ListPage/RoomListPage.vue
+++ Frontend/src/pages/ListPage/RoomListPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d08e77f0c909ea2c5db4d4a8d32ec1849a1a270ff99aa74b7560158aef8b707
-size 3056
+oid sha256:41dd6b1f10075be2c587e1d783a5171cbe990b0261249460c5e3aae4b474902b
+size 3006
