924c71317292af6ba985c33bf3621b467a7fc3cb
이하은 <hahi1108@gmail.com>
2024-02-11 17:30:15 +0900

    feat : 파일 전송용 axios 추가
    Former-commit-id: e5494630053190b83d19402d55e6df6de3aaa7e9

--- Frontend/src/axios/http-commons.js
+++ Frontend/src/axios/http-commons.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:967d7565d0e1312536d3a7ebfb604cc892e42d2755e1a47c1833b4dd7cb8ed60
-size 808
+oid sha256:75765eeef73d0e2c9896aaf57baa8341f28b35442316d9fab2537dc97f25198a
+size 1048
