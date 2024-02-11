55c765487431430984817a49e2a817609b00b05f
이하은 <hahi1108@gmail.com>
2024-02-11 17:45:06 +0900

    feat : 마이페이지 본인 프로필 반영
    Former-commit-id: 3d0488d270476a0a3a617e31d9a685aac6fab4c4

--- Frontend/src/layouts/MyPageLayout.vue
+++ Frontend/src/layouts/MyPageLayout.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c80d60056a2095c489e61a3c0112dbba7eb7a8338a9a7b3fc7e68a70c3eba684
-size 3102
+oid sha256:28dff9b402ddb55a993b8f1f39687a70686bff81e28d76a8bb34235584cd3fd3
+size 3180
