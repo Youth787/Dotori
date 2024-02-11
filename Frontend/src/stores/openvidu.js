d149c45b76f9c32798114b5439d5c3162a9eef2a
doongdang <whtjrgus0428@gmail.com>
2024-02-11 14:41:20 +0900

    mod : sessionStorage 사용
    Former-commit-id: 34848e703fb1f4d0932e9bd97def4fd8590ade15

--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a323e4c892c65c8a8bd1ca6829e339c4fa376ac7ccc9e2509aa66cec24f8de53
-size 8286
+oid sha256:82bfee8230a8887fab1b53f2bff57b9208392d7a9d04cac03bb3676c3e031c63
+size 8360
