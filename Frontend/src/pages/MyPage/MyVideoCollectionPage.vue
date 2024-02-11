f16444f70ae8458dada2fa3b97fbdd647fa92207
doongdang <whtjrgus0428@gmail.com>
2024-02-11 14:41:08 +0900

    feat : 비디오 조회 및 다운로드
    Former-commit-id: f74a2cfa2d1162c7331113e9bf494a7c6655ac0b

--- Frontend/src/pages/MyPage/MyVideoCollectionPage.vue
+++ Frontend/src/pages/MyPage/MyVideoCollectionPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:870948ac69d5bf05821c5776b9dc2265388bc8eaaca75759ad33877770df4830
-size 3885
+oid sha256:d1a913992946f482c85edf6a8759071b5c329ed6951d383bc50e7a831ed1e3ca
+size 2461
