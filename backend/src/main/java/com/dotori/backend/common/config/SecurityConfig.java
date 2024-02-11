5badfa75f630282d4cd0a97ab8cc89e1358f63f4
doongdang <whtjrgus0428@gmail.com>
2024-02-11 20:46:29 +0900

    mod : 회원 이미지 수정 API 변경
    Former-commit-id: 0ec3e26f5e40ce542bd68acd3c8e50b1016d7b2b

--- backend/src/main/java/com/dotori/backend/common/config/SecurityConfig.java
+++ backend/src/main/java/com/dotori/backend/common/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:874eb38893a21dcf9265140c679ec29e3f3c40cd7dfeb06bcc4f50e8948a51e9
-size 6488
+oid sha256:d72e88636d99349b9ba6fd07b74631ba464c2b43776b2c8fa84cc0a732daa364
+size 6484
