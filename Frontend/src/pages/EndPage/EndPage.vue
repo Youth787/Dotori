829e6a726e16a4cd20800bf2083fd5cf59ce3623
jinooyang <jinooyang@gmail.com>
2024-02-12 14:58:19 +0900

    feat : endpage video
    Former-commit-id: da34b320394a47079bb1b887cb6e21deffb67f18

--- Frontend/src/pages/EndPage/EndPage.vue
+++ Frontend/src/pages/EndPage/EndPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db3b24a40d3ab7c4bd8e21031eb7d00d78621d1175a0dd64c521c89185946fac
-size 1890
+oid sha256:6e99900ba4876291d607980c2b04ae1155335a6afd3012066e24104e2da29bea
+size 3856
