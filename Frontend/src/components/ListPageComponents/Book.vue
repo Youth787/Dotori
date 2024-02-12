88a9fd298cdb8bb2905a888fb49ff2254c420f7e
이하은 <hahi1108@gmail.com>
2024-02-12 10:56:03 +0900

    design : 방, 책 리스트 컴포넌트
    Former-commit-id: 0b9a58b0808cf56837274b8cc0ca0be477805525

--- Frontend/src/components/ListPageComponents/Book.vue
+++ Frontend/src/components/ListPageComponents/Book.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7296c0c64ea5832f4ee0face26a3d6c68a7a07db3aa231a558e64649fc0b7eac
-size 1810
+oid sha256:6ee239ccd5618b49d0d12faa5d04ecb7f4b4ec1c20b2c4af0feaecfbb1552700
+size 1832
