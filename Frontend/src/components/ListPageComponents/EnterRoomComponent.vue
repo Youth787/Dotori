37ad9a787199f92821d214b3cef7c1d81a0913f8
yeonmi <ar77gt@daum.net>
2024-02-12 23:27:12 +0900

    design
    Former-commit-id: 864c02e3e7bd648a1316d1e99996502b6b487301

--- Frontend/src/components/ListPageComponents/EnterRoomComponent.vue
+++ Frontend/src/components/ListPageComponents/EnterRoomComponent.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a615791dcb9dbeffd48bb065f89bc40cef95e9f6065887d136a1192dbae14412
-size 3370
+oid sha256:d2054f3eb21e665309c3995f8dc8bbb2ce20d899c20fcde8d6624bf7bb9f9f20
+size 3391
