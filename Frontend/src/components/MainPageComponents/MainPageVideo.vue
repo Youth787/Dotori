ec0d8242a0de8c9c77a8c4bd2e79e0d054ddf7a6
이하은 <hahi1108@gmail.com>
2024-02-12 16:45:20 +0900

    design : 메인페이지 시작 버튼
    Former-commit-id: 963d99fd541d22ae23d2bce6e813e3050b51c52f

--- Frontend/src/components/MainPageComponents/MainPageVideo.vue
+++ Frontend/src/components/MainPageComponents/MainPageVideo.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05819e29b883e3b6f1dae019683ed07851eaf51ad4abb383b2ca488261bae07d
-size 2729
+oid sha256:bfb15bdce8fa117830d3b2c423def22ddcba52af9e68c3fadad6fcf1411b5613
+size 2488
