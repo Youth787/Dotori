cbd29503a5c412ae5f4f8c1415abb5e6d3236288
yeonmi <wkdus885@naver.com>
2024-02-12 23:27:12 +0900

    design
    Former-commit-id: 864c02e3e7bd648a1316d1e99996502b6b487301

--- Frontend/src/components/MainPageComponents/MainPageVideo.vue
+++ Frontend/src/components/MainPageComponents/MainPageVideo.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:641b8df2f4df0e980bf5f280799935afad463e8f5b46b02c1b738f39df3e41bb
-size 2477
+oid sha256:65ca86df529489c35f39840d1f993703180ee25e72a445caa996b08d5c90ad3c
+size 4254
