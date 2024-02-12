9dc4388f9c3dac7e5b06386077a42fd95b8cfa2e
yeonmi <wkdus885@naver.com>
2024-02-12 17:17:35 +0900

    color : my info
    Former-commit-id: 0bc269dad8fe40b49cd26ea4f913e747473cf990

--- Frontend/src/pages/MyPage/MyInfoPage.vue
+++ Frontend/src/pages/MyPage/MyInfoPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef233f247dc9f3fccb7c125638710e56d7cbb1446ca5925b3ff236c736cbd410
-size 5072
+oid sha256:8893af05d607770f16a236af72e126f149f669d5903ee9633ab83786d770cef5
+size 5090
