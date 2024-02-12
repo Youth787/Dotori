44798cace7d9d4d64cd8d0a28d7a4d65f03fa4a8
이하은 <hahi1108@gmail.com>
2024-02-12 16:48:16 +0900

    rebase
    Former-commit-id: 4d5e3b3ba49f5f8ca2ec67e702f04dafe3bd4be8

--- Frontend/src/components/RecordingPageComponents/SideBarComponent.vue
+++ Frontend/src/components/RecordingPageComponents/SideBarComponent.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0309113b045dd62c3a63e456bab9638d7d185d7c287aef064f40538b6d065845
-size 3093
+oid sha256:63bda443777b40a584ca83e513045980f5eb2ddb13df20b2bdfdad06aa5c98ef
+size 3074
