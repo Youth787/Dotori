5097d4a42593bed949deb445fd2e495c44ec7608
yeonmi <ar77gt@daum.net>
2024-02-12 22:12:09 +0900

    design : roomlist page
    Former-commit-id: 2c9ee379112a19194b804ad7fd7c498da96563d2

--- Frontend/src/components/ListPageComponents/EnterRoomComponent.vue
+++ Frontend/src/components/ListPageComponents/EnterRoomComponent.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2a0c41fbdf927d843a5342c5c915b433073182277652a214ff1a34c2fcdda5d
-size 3207
+oid sha256:a615791dcb9dbeffd48bb065f89bc40cef95e9f6065887d136a1192dbae14412
+size 3370
