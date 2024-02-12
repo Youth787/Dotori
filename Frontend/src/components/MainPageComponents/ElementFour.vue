a9e73ee99bfee6ff4c3547335c6be95fbd75e35c
이하은 <hahi1108@gmail.com>
2024-02-12 20:34:38 +0900

    design : 글귀
    Former-commit-id: f8331d4d7d77cf26e282bb069c44970f538b8494

--- Frontend/src/components/MainPageComponents/ElementFour.vue
+++ Frontend/src/components/MainPageComponents/ElementFour.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c0795aa47a20831ee722c6b8eb4a599afa00a52b5b6bf8dc18f5fed1a143d36
-size 1020
+oid sha256:293175c80565de5605a0f022cf6dd4393a6dc0d40ed436343de176d4a490a00a
+size 2843
