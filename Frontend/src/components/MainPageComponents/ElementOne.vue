45e0a1912e9b51c683f40f75f8c5017f65230d6a
yeonmi <ar77gt@daum.net>
2024-02-12 21:51:44 +0900

    design : roomlist, mainpage
    Former-commit-id: 844410f85d6cca1b2f0298b4680cc36c36e6710b

--- Frontend/src/components/MainPageComponents/ElementOne.vue
+++ Frontend/src/components/MainPageComponents/ElementOne.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6fe6a48116f161502debde226f2296731afdac23be8725d7017159a1a8eb453
-size 1210
+oid sha256:ca6dc56543114b967d3c2b913b33a6af25340fa12d5cd56f59061fb1bd682523
+size 1209
