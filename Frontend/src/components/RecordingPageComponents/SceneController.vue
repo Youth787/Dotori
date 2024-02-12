04ae53ca3ab6e4ed2e06266b73fa974a4ae0eecb
jinooyang <jinooyang@gmail.com>
2024-02-12 14:42:01 +0900

    fix : 방장 연극 종료 시  unpublish 안되는 버그 수정,녹화중에 페이지 이동 클릭 시 모두에게 불가능 메세지 보내던 현상 수정
    Former-commit-id: 15f26656d1e0751225691d718f703417f12cdd9d

--- Frontend/src/components/RecordingPageComponents/SceneController.vue
+++ Frontend/src/components/RecordingPageComponents/SceneController.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f11c0ca23c017af9dd3c282661d04cdc8e4e349720841651f4f8807a9873c7c8
-size 7874
+oid sha256:fdbde0f5269afa9367476b009e19a955cd95f4db27222635b15abbce3dfa079d
+size 7925
