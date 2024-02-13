c7738a01cf9646a800565a03e802b31192ba80cc
jinooyang <jinooyang@gmail.com>
2024-02-13 16:17:10 +0900

    fix : recording success bug
    Former-commit-id: e52c9ae7e150887a8646baf5c69cd7c7a1d7b4d1

--- Frontend/src/components/RecordingPageComponents/SceneController.vue
+++ Frontend/src/components/RecordingPageComponents/SceneController.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8f977b98fc859058aae3d6388d96cf2b7b860b2f3df33fd332d45ffb1a038dd
-size 8001
+oid sha256:d670afa4bb7021ff45ef28268f12317d985ed9b1168e6b343802a08b37c9715e
+size 8251
