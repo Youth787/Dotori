13e421f7d09ccdb12ea16cb69cfeded8c38b9819
yeonmi <ar77gt@daum.net>
2024-02-13 15:16:28 +0900

    design
    Former-commit-id: 6ca2ed7e4d3d8e9c8c4f23cf72df993465a05a5f

--- Frontend/src/pages/ListPage/BookListPage.vue
+++ Frontend/src/pages/ListPage/BookListPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb49d98b5a768c1ace1cbf72eefbf0edba8204dc5c5e044f787f2ff19c92d676
-size 3328
+oid sha256:bfd565c13ca6a2fed059c2f4ab8f698fc6d4dd740592ac1696af15ed9e9f6dbd
+size 3707
