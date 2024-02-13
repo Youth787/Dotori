f26455715415a337022859bc6087ef8137a20e58
jinooyang <jinooyang@gmail.com>
2024-02-13 09:04:15 +0900

    fix : bookList페이지 넘어가는거 수정
    Former-commit-id: 575bdf5e7c9cba337fbb124995a31ffca7ba5db3

--- Frontend/src/pages/ListPage/BookListPage.vue
+++ Frontend/src/pages/ListPage/BookListPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d256feaea42eee40e4ce261f6e3dfdbf57d28dd9fd85d0b7d7b837c112c6a41
-size 3332
+oid sha256:703828266f0b250476a445ed65a52aa8d0d4efa7d6ffdca40c9e78784e3afc20
+size 3314
