983d21d24ff6ae954bee1513339eed48cb89595d
jinooyang <jinooyang@gmail.com>
2024-02-13 22:03:07 +0900

    waiting room desigh fix
    Former-commit-id: 1af1cbb0d0b26f502e1cedca67cb7f6b2a5ddb7f

--- Frontend/src/pages/RoomPage/WaitingRoomPage.vue
+++ Frontend/src/pages/RoomPage/WaitingRoomPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b33443b7bf0ca1aa28e794c8dc0b2d0758f52d829681ca66133a6b263fb64cad
-size 4369
+oid sha256:9192d0efed148dee293de4405bcbce5acbf070a8e426bccaa7e814d930d02d1e
+size 4411
--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:482c4f8f766145bbed98fad7b5cc59a80f78918d62a9922985baf3fd713d1fd0
-size 8474
+oid sha256:38d88f132c20631d9869cde696390b17cea9dc565ee2912c8ba4902fb3dab4d0
+size 8519
