3128764b9bed9321ab5bb6084f5e4beebb0a8e28
문준형 <moonabcd@naver.com>
2024-02-13 14:48:43 +0900

    fix : email이 같으면 로그인이 안되는현상 수정(DB nickname 유니크설정해제,member table 재설정필요)
    Former-commit-id: e7ba28286551b28e91106b83a2bec0bd94cdd1f5

--- backend/src/main/java/com/dotori/backend/domain/member/model/entity/Member.java
+++ backend/src/main/java/com/dotori/backend/domain/member/model/entity/Member.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f35099f24aa18757382dc5ef859538a3770ef1daff10bb1c61147843c22fb44
-size 2504
+oid sha256:10bd9b008865ef6829102e0a51ff064a256cefb988ec44d58ff72004ba9fbacc
+size 2418
--- backend/src/main/java/com/dotori/backend/domain/member/service/CustomOAuth2UserService.java
+++ backend/src/main/java/com/dotori/backend/domain/member/service/CustomOAuth2UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3d1a0856734bb6132f80ad6f8403fe714e5255c183ebfaf1688b7633c5388fe
-size 4834
+oid sha256:24abc962a71ab3b8c721b2af857ca621dfcf67316e342bccecd6a7483cfca4ac
+size 4670
