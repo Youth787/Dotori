280bca2bdde9f1cb85ee93fca09fd7e5d7affdbc
jinooyang <jinooyang@gmail.com>
2024-02-13 17:14:20 +0900

    fix : unpublish (recording finished)
    Former-commit-id: 11c3b33ab59f163aa4993d7b850e16d1d13e380a

--- Frontend/src/stores/openvidu.js
+++ Frontend/src/stores/openvidu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82bfee8230a8887fab1b53f2bff57b9208392d7a9d04cac03bb3676c3e031c63
-size 8360
+oid sha256:482c4f8f766145bbed98fad7b5cc59a80f78918d62a9922985baf3fd713d1fd0
+size 8474
