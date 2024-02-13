abce18502f0026d13db0a99d932104c6bfd4f904
jinooyang <jinooyang@gmail.com>
2024-02-13 16:10:23 +0900

    fix : recording success bug
    Former-commit-id: df02a18ec4a8db5df7d69c4fe52e3a94f7317ce6

--- Frontend/src/pages/RecordingPage/RecordingRoom.vue
+++ Frontend/src/pages/RecordingPage/RecordingRoom.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1da324b2dc8e6f7a82ab86b8b5e1eb23a62705a45343e1c5f8862810802c756a
-size 23148
+oid sha256:12a75d08b84b3b5aa16dbf44a1a183a22ff66d003d47dab924df1ab72560b7b7
+size 23142
