d4076714874a063986c099afcd817aad98c84354
이하은 <hahi1108@gmail.com>
2024-02-14 09:41:11 +0900

    design : 메인페이지
    Former-commit-id: bea8527a83d9e61a65ced5eb5db31354495c5b0a

--- Frontend/src/components/MainPageComponents/MainPageVideo.vue
+++ Frontend/src/components/MainPageComponents/MainPageVideo.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:033f21d1e83670f85d652b17fd7f7aeb41346d078f1c93d768728ed63a194a0a
-size 4314
+oid sha256:fac422ab4772f456cd82fa9044f41e007a23b83f8b6e50e1e4f0da0845fba1b5
+size 4315
