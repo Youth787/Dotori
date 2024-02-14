253ac307205fd2359b8589f56679af74263d63f9
이하은 <hahi1108@gmail.com>
2024-02-14 11:21:55 +0900

    feat : 연극 종료 후 홈 or 마이페이지로 이동 시 db 방 참여자에서 제거
    Former-commit-id: 3ae1ad86d84520eeb29fe90508ac11ebb20c3af7

--- Frontend/src/pages/EndPage/EndPage.vue
+++ Frontend/src/pages/EndPage/EndPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:139993a8426e7f99c59b1144b368e8f762bfdc0e192ee067f7d42181478ab80f
-size 4372
+oid sha256:b42d83225d49faa7db1d225d4469bc406d1ab6126b1ee4f780fc336354085641
+size 4226
