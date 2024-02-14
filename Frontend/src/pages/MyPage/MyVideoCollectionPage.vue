f6d351ca0e51d2192a51b47e420726dbea66dba8
doongdang <whtjrgus0428@gmail.com>
2024-02-14 22:52:54 +0900

    feat : 미리보기
    Former-commit-id: 1081e30e08a0d86694c80e1d10f6c736487ea3ff

--- Frontend/src/pages/MyPage/MyVideoCollectionPage.vue
+++ Frontend/src/pages/MyPage/MyVideoCollectionPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e649c8a9e9cd184aea6aa090ca7205fc5087790bdeeafdc9f3a7e0acbdb1866
-size 2519
+oid sha256:89e551c57bf948cdb81b3e3b8b24d87537258a636a08da31026b6baafdac75f6
+size 3171
