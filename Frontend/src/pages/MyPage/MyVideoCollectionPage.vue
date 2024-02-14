054fa15176bfcf28eea2487d32eb47ee19d1acd1
doongdang <whtjrgus0428@gmail.com>
2024-02-14 23:02:06 +0900

    remove : / 중복
    Former-commit-id: c4868db287f2de592b1642bab9eae681c023af45

--- Frontend/src/pages/MyPage/MyVideoCollectionPage.vue
+++ Frontend/src/pages/MyPage/MyVideoCollectionPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89e551c57bf948cdb81b3e3b8b24d87537258a636a08da31026b6baafdac75f6
-size 3171
+oid sha256:a100f2bad6d15cd66b549ef15580a8b923afe8824d9e34a0c385444116dd3cc7
+size 3172
