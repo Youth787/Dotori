afaf0fa4b774b08cc7c29918600aa04eca560f3a
yeonmi <ar77gt@daum.net>
2024-02-14 10:24:05 +0900

    design : login
    Former-commit-id: 2e424d37adcabbef4149f2f6738e7bbf5dfa20ff

--- Frontend/src/pages/LoginPage/LoginPage.vue
+++ Frontend/src/pages/LoginPage/LoginPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6881a99b22d3e053702b8121ee5b14cc9fb30781131336857da0cdfa91896fc
+oid sha256:af07ab4a1ea2e99508ef7e24e59c3c88a48edbf06db1f2ffe6b0827be6cd9e0c
 size 3037
