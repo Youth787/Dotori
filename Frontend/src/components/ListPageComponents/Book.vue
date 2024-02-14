367ffc4b8619e0df71cfb060511cd18b10d356d1
이하은 <hahi1108@gmail.com>
2024-02-14 09:35:06 +0900

    design : 방 생성 모달 디자인 수정
    Former-commit-id: 6d64dced26515fb8b3b99fab5d5c463d336ad31a

--- Frontend/src/components/ListPageComponents/Book.vue
+++ Frontend/src/components/ListPageComponents/Book.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f17e24150acfd45aaa710d6a42fba7e2ab45c93c0ed15bf2f26a8e307a1f7e25
-size 2306
+oid sha256:6bc5958a224d28e4192db4ee56250bdfa3fc3cb65fa5f45e1e59a939aff5420c
+size 2047
--- Frontend/src/pages/ListPage/NewRoomModal.vue
+++ Frontend/src/pages/ListPage/NewRoomModal.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:826d3117a946234bfab5c166dc5a8255a4d381a01569f9bfad04478970a84689
-size 7732
+oid sha256:531c9b3c6af9f76607a2d510161f491e7ebb597169f6ea482b7de98d9d45062a
+size 7908
