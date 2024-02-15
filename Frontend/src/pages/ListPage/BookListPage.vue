592903f6151cd7670e51b82be6d2cd6231e14e99
doongdang <whtjrgus0428@gmail.com>
2024-02-15 10:48:46 +0900

    mod : pointer 제거
    Former-commit-id: db18a795abe326a9c0029a46af3725a2383336c2

--- Frontend/src/pages/ListPage/BookListPage.vue
+++ Frontend/src/pages/ListPage/BookListPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed777a06d821e033d548521009b6f841b2e231fcb87785cdf60bdaded0b677a7
-size 3784
+oid sha256:c0cd6128dc860e1b28e501ad176ddfa329b0cb1cde4e302ceaea2bbcba640695
+size 3763
