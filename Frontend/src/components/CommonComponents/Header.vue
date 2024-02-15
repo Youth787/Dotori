6b59568422f914e4fe630cbf3d0b7afcd9bb8b24
doongdang <whtjrgus0428@gmail.com>
2024-02-15 12:06:38 +0900

    mod : 로그인 라우터 수정
    Former-commit-id: 6c8b01a65e9e055908512029e4ef3dc462dc3ddd

--- Frontend/src/components/CommonComponents/Header.vue
+++ Frontend/src/components/CommonComponents/Header.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c146e9f64ed024fb0a43194a4b612a624b7f36423d3f90f1153a89c5bccc8c5
-size 5696
+oid sha256:2493f6c87828e16165b7f7e50a58f35f27d0636f0fe04bf61d624d5116e26e0c
+size 5701
--- Frontend/src/components/CommonComponents/Headermainpage.vue
+++ Frontend/src/components/CommonComponents/Headermainpage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1313ca20b26663471a0fc70375894ea4ee0991fa700bcc67ce2cf70b1af9d030
-size 5940
+oid sha256:a32174115c4ff2de7f6b016d856364492ca0597e8b985ade0e767172a599f094
+size 5945
