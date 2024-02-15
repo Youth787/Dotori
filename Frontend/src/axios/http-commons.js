5bf59419ddece492e6f91b396e74e89e0f75df2a
doongdang <whtjrgus0428@gmail.com>
2024-02-15 10:50:21 +0900

    mod : axios basce url .env화
    Former-commit-id: 11955448c3d7e250e5634aa967b1a932046ebfdf

--- Frontend/src/axios/http-commons.js
+++ Frontend/src/axios/http-commons.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66f5723c24433f192c563dff3ddc184bd399eabdcec1920e40ffd91df137f3b5
-size 2301
+oid sha256:0dfa33d0f3b84f5031d6260d996551e21a08e17cd1603cc3f2e29112d3db59f1
+size 2272
--- Frontend/src/pages/LoginPage/LoginPage.vue
+++ Frontend/src/pages/LoginPage/LoginPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e867d94543814d14dbd17c9fddcc5e24d790facbb20893f73fe41ef59d819fba
-size 2963
+oid sha256:dc66d76aaea700fc750051dbd8daf93626d5e169ea496ce7f7918039b4513085
+size 2960
