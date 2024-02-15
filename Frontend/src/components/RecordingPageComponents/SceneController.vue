b58c1f82c229be1e3a4f4bc8570f6b0137b30968
doongdang <whtjrgus0428@gmail.com>
2024-02-15 12:07:18 +0900

    mod : openvidu rest api분리
    Former-commit-id: 4a5574756f730afd45b442dab5fa6c25ddc23e55

--- Frontend/src/components/RecordingPageComponents/SceneController.vue
+++ Frontend/src/components/RecordingPageComponents/SceneController.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52ceb36befdb2a41062defbe2f61ee78bd3bf35bf16354c55f7802d93d2ffc0c
-size 8165
+oid sha256:f1f0a47417bf3f1b112159ac8c685c863d7a5090a97f54a7c160bc10e71f45c2
+size 8019
