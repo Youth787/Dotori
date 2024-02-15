82682daee3b06339bed254a7e941a0a4beff2bff
jinooyang <jinooyang@gmail.com>
2024-02-15 09:32:20 +0900

    login fix
    Former-commit-id: 14392a9f2084898dd2f4bd1aea228417e6551cf5

--- Frontend/src/pages/LoginPage/LoginPage.vue
+++ Frontend/src/pages/LoginPage/LoginPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c837d585708ff15ab203177fa1d13aef4ef226ad34e5899d298275d6ef7f348
+oid sha256:1fcac9858364adc09b68d3f26f06aa4c7814fbc736ca8f743f3f919bf89eab90
 size 2959
