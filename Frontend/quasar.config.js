3b7e246111e0210cbb9bcabe8d5f0460eb1072eb
doongdang <whtjrgus0428@gmail.com>
2024-02-15 12:07:18 +0900

    mod : openvidu rest api분리
    Former-commit-id: 4a5574756f730afd45b442dab5fa6c25ddc23e55

--- Frontend/quasar.config.js
+++ Frontend/quasar.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8480fc6bea8f79834cf5bf48491198800aaf6247cad3de27dece42375d8ba68
-size 7014
+oid sha256:bcaac78757b5c59bb1dc4408f463afd81a819681b6c516f708180a933d6151f5
+size 7077
--- Frontend/src/axios/http-commons.js
+++ Frontend/src/axios/http-commons.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0dfa33d0f3b84f5031d6260d996551e21a08e17cd1603cc3f2e29112d3db59f1
-size 2272
+oid sha256:f1a5b63188f9d97652ec0f8da70e83aee433808335f3ce8585c5f2519eddc234
+size 2201
